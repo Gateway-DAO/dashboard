07a4c31f14823b9d59e0f87504c31c6ed0e3346c
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-08-14 14:44:23 -0300

    feat: i18n

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+const nextTranslate = require('next-translate-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = nextTranslate(nextConfig)
