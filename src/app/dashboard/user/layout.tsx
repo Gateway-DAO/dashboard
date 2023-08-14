edfd8bb87e91678d43a2046ebd001ad781ba8d12
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-08-14 20:39:05 -0300

    fix: layout name

--- src/app/dashboard/user/layout.tsx
+++ src/app/dashboard/user/layout.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from "react";
 
 import DashboardLayout from "../components/dashboard-layout";
 
-export default function DashboardOrganizationLayout({ children }: PropsWithChildren) {
-  return <DashboardLayout content={<><div>Organization</div></>}>
+export default function DashboardUserLayout({ children }: PropsWithChildren) {
+  return <DashboardLayout content={<><div>User</div></>}>
     {children}
   </DashboardLayout>
 }
