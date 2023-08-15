e6d61451987384f1d9788b0904462227bf8e969b
João Conserva <joao@mygateway.xyz>
2023-08-15 10:54:50 -0500

    fix: fixed notistack

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,19 +1,20 @@
-'use client'
+'use client';
 
-import { PropsWithChildren, useState } from 'react'
+import { PropsWithChildren, useState } from 'react';
 
-import { queryClientConfig } from '@/services/query-client'
-import { ThemeProvider } from '@/theme'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Notistack from '@/components/notistack/notistack';
+import { queryClientConfig } from '@/services/query-client';
+import { ThemeProvider } from '@/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function Providers({ children }: PropsWithChildren<{}>) {
-  const [queryClient] = useState(() => new QueryClient(queryClientConfig))
+  const [queryClient] = useState(() => new QueryClient(queryClientConfig));
 
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        {children}
+        <Notistack>{children}</Notistack>
       </ThemeProvider>
     </QueryClientProvider>
-  )
+  );
 }
