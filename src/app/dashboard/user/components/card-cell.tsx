167e3a2809bc92119f6f04f5cc452edcada432b1
João Conserva <joao@mygateway.xyz>
2023-08-15 10:05:24 -0500

    feat: created tda component

---
+++ src/app/dashboard/user/components/card-cell.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+type Props = {
+  label: string;
+  children: ReactNode;
+};
+
+export default function CardCell({ label, children }: Props) {
+  return (
+    <Stack
+      gap={1}
+      sx={{
+        p: 2,
+        width: '100%',
+        fontSize: 12,
+        whiteSpace: 'pre-wrap',
+        wordBreak: 'break-word',
+      }}
+    >
+      <Typography fontSize={14}>{label}</Typography>
+      <Box sx={{ display: 'inline-flex' }}>{children}</Box>
+    </Stack>
+  );
+}
