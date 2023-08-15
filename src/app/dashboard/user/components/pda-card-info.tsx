12868983f5283adcdd06b1e48895c118ee672d2a
João Conserva <joao@mygateway.xyz>
2023-08-15 14:10:32 -0500

    fix: fixed pda card info

--- src/app/dashboard/user/components/pda-card-info.tsx
+++ src/app/dashboard/user/components/pda-card-info.tsx
@@ -2,7 +2,7 @@ import CopyPaste from '@/components/copy-paste/copy-paste';
 import { limitCharsCentered } from '@/utils/string';
 import dayjs from 'dayjs';
 
-import { Stack, Box, Divider, Chip } from '@mui/material';
+import { Stack, Divider, Chip } from '@mui/material';
 
 import CardCell from './card-cell';
 type Props = {
@@ -15,50 +15,37 @@ export default function PdaCardInfo({ credential }: Props) {
       sx={{
         border: '1px solid',
         borderColor: 'divider',
-        borderRadius: 2,
+        borderRadius: 1,
         mb: 3,
         overflow: 'hidden',
         boxShadow: 'none',
-        maxWidth: 550,
       }}
       divider={<Divider sx={{ width: '100%' }} />}
     >
-      <Stack
-        alignItems="stretch"
-        justifyContent="space-around"
-        direction="column"
-        divider={
-          <Box>
-            <Divider />
-          </Box>
-        }
-      >
-        {/* TODO: Add dynamic information */}
-        <CardCell label="Share date">
-          {dayjs('2018-04-04T16:00:00.000Z').format('MM/DD/YYYY, h:mm A')}
-        </CardCell>
+      {/* TODO: Add dynamic information */}
+      <CardCell label="Share date">
+        {dayjs('2018-04-04T16:00:00.000Z').format('MM/DD/YYYY, h:mm A')}
+      </CardCell>
 
-        {/* TODO: Add dynamic information */}
-        <CardCell label="Data Proof ID">
-          <CopyPaste
-            text={limitCharsCentered(
-              '7Cae5130c16e6c8b686440b900d93fe1291977e70b812d170024f1cffd0e3fe375',
-              8
-            )}
-          />
-        </CardCell>
+      {/* TODO: Add dynamic information */}
+      <CardCell label="Data Proof ID">
+        <CopyPaste
+          text={limitCharsCentered(
+            '7Cae5130c16e6c8b686440b900d93fe1291977e70b812d170024f1cffd0e3fe375',
+            8
+          )}
+        />
+      </CardCell>
 
-        {/* TODO: Add dynamic information */}
-        <CardCell label="Status">
-          <Chip
-            label="Up-to-date"
-            size="small"
-            variant="outlined"
-            color="success"
-            sx={{ color: 'success.main' }}
-          />
-        </CardCell>
-      </Stack>
+      {/* TODO: Add dynamic information */}
+      <CardCell label="Status">
+        <Chip
+          label="Up-to-date"
+          size="small"
+          variant="filled"
+          color="success"
+        />
+      </CardCell>
     </Stack>
   );
 }
