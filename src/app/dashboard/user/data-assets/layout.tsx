acc989ece937a1db9d51651e9bd75c46503f3203
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-08-15 20:22:58 -0300

    feat: template margin

--- src/app/dashboard/user/data-assets/layout.tsx
+++ src/app/dashboard/user/data-assets/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 import GTWTab from "@/components/tabs/gtw-tab";
 import GTWTabs from "@/components/tabs/gtw-tabs-links";
-import { NEGATIVE_CONTAINER_PX } from "@/theme/config/style-tokens";
+import { CONTAINER_PX, NEGATIVE_CONTAINER_PX } from "@/theme/config/style-tokens";
 
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
@@ -11,8 +11,14 @@ export default function DataAssetsLayout({ children }: PropsWithChildren) {
 
   return (
     <>
-      <Typography variant="h3">My data access</Typography>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider', ml: NEGATIVE_CONTAINER_PX }}>
+      <Typography variant="h3" sx={{
+        mt: 7, mb: {
+          xs: 4,
+          md: 5,
+          lg: 6
+        }
+      }}>My data access</Typography>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', mx: NEGATIVE_CONTAINER_PX, px: CONTAINER_PX }}>
         <GTWTabs>
           <GTWTab label="Private" href="/dashboard/user/data-assets" />
           <GTWTab label="Shared With" href="/dashboard/user/data-assets/shared-with" />
