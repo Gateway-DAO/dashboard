e1416d730d1c638b2b6374505fa42551c4d86487
Lucas Inacio @kbooz <lucas1inacio7souza@gmail.com>
2023-08-17 20:57:22 -0300

    feat: link

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+
+  return <>
+    <Link href="/dashboard/user">
+      Go to dashboard
+    </Link>
+  </>;
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import useTranslation from 'next-translate/useTranslation';
-
-export default function Home() {
-  const { t } = useTranslation('home');
-
-  return <>{t('title')}</>;
-}
