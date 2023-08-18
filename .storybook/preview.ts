92db6b4be9d831d93298639e24d39c44ffc58e8e
Lucas Inacio @kbooz <lucas1inacio7souza@gmail.com>
2023-08-18 13:11:32 -0300

    wip: storybook typescript

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,37 +1,33 @@
 import type { Preview } from '@storybook/react';
 
+import { theme } from '../src/theme/theme';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { withThemeFromJSXProvider } from '@storybook/addon-styling';
-import {theme} from "../src/theme/theme"
-/* TODO: update import for your custom Material UI themes */
-// import { lightTheme, darkTheme } from '../path/to/themes';
 
-// Import your fontface CSS files here
-// Don't have any? We recommend installing and using @fontsource/roboto
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles: CssBaseline,
+    // Uncomment for theme switching
+    Provider: ThemeProvider,
+    themes: {
+      light: theme,
+    },
+    defaultTheme: 'light',
+  }),
+];
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
+      expanded: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
     },
   },
-
-  decorators: [
-    // Adds global styles and theme switching support.
-    withThemeFromJSXProvider({
-      GlobalStyles: CssBaseline,
-      // Uncomment for theme switching
-      Provider: ThemeProvider,
-      themes: {
-        light: theme,
-      },
-      defaultTheme: 'light',
-    }),
-  ],
 };
 
 export default preview;
+
---
+++ src/stories/Chip.component.tsx
@@ -0,0 +1,10 @@
+
+import { Chip as MuiChip, ChipProps as MuiChipProps } from '@mui/material';
+
+
+
+export interface ChipProps extends MuiChipProps { }
+const Chip = ({ ...rest }: ChipProps) => <MuiChip {...rest} />;
+
+export default Chip;
+
