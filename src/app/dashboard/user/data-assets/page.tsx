b6cf0b7e9272744bb0a4c365ef2d589b5cb3821c
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-08-22 17:04:21 -0300

    feat: split error loading to page

--- src/app/dashboard/user/data-assets/page.tsx
+++ src/app/dashboard/user/data-assets/page.tsx
@@ -1,18 +1,7 @@
-import { Suspense } from 'react';
-
-import ErrorBoundary from '@/components/error-boundary/error-boundary';
-
-import { Typography } from '@mui/material';
-
 import PDAsList from './components/pdas-list';
-import PDAsListSkeleton from './components/pdas-list-skeleton';
 
 export default function DataAssetsPage() {
   return (
-    <ErrorBoundary fallback={<Typography>Suspense error boundary</Typography>}>
-      <Suspense fallback={<PDAsListSkeleton />}>
-        <PDAsList />
-      </Suspense>
-    </ErrorBoundary>
+    <PDAsList />
   );
 }
