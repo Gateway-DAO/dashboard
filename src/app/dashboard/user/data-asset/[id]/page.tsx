e3aa183a71a8cae6c9ea99aa21c3df55ab2c4b00
João Conserva <joao@mygateway.xyz>
2023-08-23 17:53:43 -0300

    chore: added top bar container

--- src/app/dashboard/user/data-asset/[id]/page.tsx
+++ src/app/dashboard/user/data-asset/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from 'react';
 
+import BackButton from '@/components/buttons/back-button';
 import ErrorBoundary from '@/components/error-boundary/error-boundary';
+import TopBarContainer from '@/components/top-bar-container/top-bar-container';
 
 import { Typography } from '@mui/material';
 
@@ -11,7 +13,12 @@ export default async function PDAPage({ params }: { params: { id: string } }) {
   return (
     <ErrorBoundary fallback={<Typography>Suspense error boundary</Typography>}>
       <Suspense fallback={<PDASkeleton />}>
-        <PDAItem id={params?.id} />
+        <>
+          <TopBarContainer>
+            <BackButton />
+          </TopBarContainer>
+          <PDAItem id={params?.id} />
+        </>
       </Suspense>
     </ErrorBoundary>
   );
--- src/app/dashboard/user/layout.tsx
+++ src/app/dashboard/user/layout.tsx
@@ -6,24 +6,12 @@ import MenuItems, { MenuBottombarItems } from './components/menu-items';
 export default function DashboardUserLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <DashboardLayout menuItems={<MenuItems />} mobileMenuItems={<MenuBottombarItems />}>
-        {/* <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-between"
-          gap={1}
-          sx={{
-            display: 'flex',
-            pt: 2,
-            flexGrow: {
-              md: 0.5,
-            },
-          }}
-        >
-        <BackButton />
-      </Stack> */}
+      <DashboardLayout
+        menuItems={<MenuItems />}
+        mobileMenuItems={<MenuBottombarItems />}
+      >
         {children}
-      </DashboardLayout >
+      </DashboardLayout>
     </>
   );
 }
