a85705bef242cc912f1588cd123e08d366cb2dbe
André Felizardo <me@andrefelizardo.com.br>
2023-08-24 14:49:22 -0300

    feat: table

--- src/app/dashboard/user/data-assets/shared-with/page.tsx
+++ src/app/dashboard/user/data-assets/shared-with/page.tsx
@@ -1,7 +1,5 @@
-import {
-  ColumnGrid,
-  TableFilterSearch,
-} from '@/components/table-filter-search/table-filter-search';
+'use client';
+import { useState } from 'react';
 
 import { FilterListOutlined, SearchOutlined } from '@mui/icons-material';
 import { IconButton, Stack, TextField } from '@mui/material';
@@ -9,6 +7,8 @@ import { IconButton, Stack, TextField } from '@mui/material';
 import { TableSharedDataAssets } from './table-shared';
 
 export default function DataAssetsSharedWithPage() {
+  const [tableView, setTableView] = useState(true);
+
   return (
     <Stack>
       <Stack
@@ -25,11 +25,14 @@ export default function DataAssetsSharedWithPage() {
           label="Search"
           InputProps={{ endAdornment: <SearchOutlined /> }}
         />
-        <IconButton sx={{ borderRadius: '48px', border: '1px solid' }}>
+        <IconButton
+          sx={{ borderRadius: '48px', border: '1px solid' }}
+          onClick={() => setTableView((prev) => !prev)}
+        >
           <FilterListOutlined />
         </IconButton>
       </Stack>
-      <TableSharedDataAssets />
+      {tableView ? <TableSharedDataAssets /> : <p>nada ainda</p>}
     </Stack>
   );
 }
