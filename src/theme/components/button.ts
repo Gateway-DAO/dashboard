bcbc64d4a9bf3511f35db7859fe07a54c0ed53bb
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-08-29 11:42:00 -0300

    feat: rounded button and unset tab text transformation

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -2,8 +2,9 @@ import { Theme } from '@mui/material/styles';
 
 export const MuiButton: Extract<Theme['components'], object>['MuiButton'] = {
   styleOverrides: {
-    root: {
+    root: ({theme}) => ({
       textTransform: 'unset',
-    },
+      borderRadius: theme.spacing(5),
+    }),
   },
 };
---
+++ src/theme/components/tab.ts
@@ -0,0 +1,9 @@
+import { Theme } from '@mui/material/styles';
+
+export const MuiTab: Extract<Theme['components'], object>['MuiTab'] = {
+  styleOverrides: {
+    root: {
+      textTransform: 'unset',
+    },
+  },
+};
