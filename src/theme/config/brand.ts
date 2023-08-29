d681c14a1fa9b839b2fcfe60d1b219162b4e0339
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-08-29 12:25:32 -0300

    feat: gateway logo color

---
+++ src/theme/config/brand.ts
@@ -0,0 +1,8 @@
+export const brandColors = {
+  primary: '#771AC9',
+  secondary: '#70ECFE',
+  error: '#FF7D6B',
+  warning: '#F9C235',
+  info: '#69DCED',
+  success: '#CDDA05',
+};
--- src/theme/config/palette.ts
+++ src/theme/config/palette.ts
@@ -1,27 +1,29 @@
 import { PaletteOptions } from '@mui/material';
 
+import { brandColors } from './brand';
+
 export const palette: PaletteOptions = {
   mode: 'light',
   primary: {
-    main: '#771AC9',
+    main: brandColors.primary,
   },
   secondary: {
-    main: "#70ECFE"
+    main: brandColors.secondary,
   },
   error: {
-    main: "#FF7D6B"
+    main: brandColors.error,
   },
   warning: {
-    main: "#F9C235"
+    main: brandColors.warning,
   },
   info: {
-    main: "#69DCED"
+    main: brandColors.info,
   },
   success: {
-    main: "#CDDA05"
+    main: brandColors.success,
   },
   background: {
     default: '#F6F4FA',
     paper: '#fff',
-  }
+  },
 };
