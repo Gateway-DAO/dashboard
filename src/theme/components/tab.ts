74b230dc188192ad8be44f95fe7bce9b8ed54b6f
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-08-29 11:47:23 -0300

    feat: tab bold

--- src/theme/components/tab.ts
+++ src/theme/components/tab.ts
@@ -3,6 +3,7 @@ import { Theme } from '@mui/material/styles';
 export const MuiTab: Extract<Theme['components'], object>['MuiTab'] = {
   styleOverrides: {
     root: {
+      fontWeight: 'bold',
       textTransform: 'unset',
     },
   },
