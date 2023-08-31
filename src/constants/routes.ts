73e9ea25ae6055c054a8cbf2cb73cddcde7f985e
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-08-31 16:59:27 -0300

    feat: proof page

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,8 +2,12 @@ const routes = {
   home: '/',
   auth: '/auth',
   dashboardUser: '/dashboard/user',
+  dashboardUserProof: '/dashboard/user/proof/[id]',
+  dashboardUserProofs: '/dashboard/user/proofs',
   dashboardUserPDAs: '/dashboard/user/data-assets',
   dashboardUserPDA: '/dashboard/user/data-asset/[id]',
+  dashboardUserDataRequests: '/dashboard/user/data-requests',
+  dashboardUserDataRequest: '/dashboard/user/data-request/[id]',
   dashboardOrg: '/dashboard/org/[id]',
 };
 
