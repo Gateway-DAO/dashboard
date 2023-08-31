1739b04b48deaedb6fd7ef02202b732b9c644966
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-08-31 16:59:27 -0300

    feat: proof page

---
+++ src/app/dashboard/user/data-request/[id]/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+import routes from "@/constants/routes";
+import { PageProps } from "@/types/next";
+
+export default function DashboardUserDataRequest({ params: { id } }: PageProps<{ id: string }>) {
+  return (
+    <>
+      <h1>DashboardUserDataRequest</h1>
+      <p>Open <Link href={{
+        query: {
+          back: routes.dashboardUserDataRequest.replace("[id]", id)
+        },
+        pathname: routes.dashboardUserProof.replace("[id]", "mock")
+      }}>proof</Link></p>
+    </>
+  )
+}
