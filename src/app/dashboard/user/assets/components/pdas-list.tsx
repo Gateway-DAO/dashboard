43542be4b5c30042ce9d2a4825ecd41bb5272423
André Felizardo <me@andrefelizardo.com.br>
2023-09-01 12:18:27 -0300

    fix: merge

--- src/app/dashboard/user/assets/components/pdas-list.tsx
+++ src/app/dashboard/user/assets/components/pdas-list.tsx
@@ -1,25 +1,42 @@
+import PdaCard from '@/components/pda-card/pda-card';
+import routes from '@/constants/routes';
+import { pdas as pdasLocales } from '@/locale/en/pda';
+import { Credential } from '@/services/protocol/types';
+import { DeepPartial } from 'react-hook-form';
 
-import PdaCard from "@/components/pda-card/pda-card";
-import routes from "@/constants/routes";
-import { pdas as pdasLocales } from "@/locale/en/pda"
-import { Credential } from "@/services/protocol/types";
-import { DeepPartial } from "react-hook-form";
+import { Typography } from '@mui/material';
 
-import { Typography } from "@mui/material";
-
-import PDAsListContainer from "./pdas-list-container";
+import PDAsListContainer from './pdas-list-container';
 
 type Props = {
-  pdas: DeepPartial<Credential>[]
-}
+  pdas: DeepPartial<Credential>[];
+};
 
 export default async function PDAsList({ pdas }: Props) {
+  console.log('PDAs internos', pdas);
   if (!pdas.length) {
-    return <Typography variant="body1" color="text.secondary" sx={{ textAlign: "center", width: "100%" }}>{pdasLocales.empty}</Typography>
+    return (
+      <Typography
+        variant="body1"
+        color="text.secondary"
+        sx={{ textAlign: 'center', width: '100%' }}
+      >
+        {pdasLocales.empty}
+      </Typography>
+    );
   }
-  return <PDAsListContainer>
-    {pdas.map((pda) => (
-      <PdaCard key={pda.id} href={routes.dashboardUserAsset.replace("[id]", pda.id!)} name={pda.title!} issuerImage={pda.image} issuerName={"GET ISSUER NAME"} status={pda.status!} />
-    ))}
-  </PDAsListContainer>
+  return (
+    <PDAsListContainer>
+      {pdas.map((pda) => (
+        <PdaCard
+          key={pda.id}
+          href={routes.dashboardUser.replace('[id]', pda.id!)}
+          name={pda.title!}
+          issuerImage={pda.image}
+          issuerName={'GET ISSUER NAME'}
+          status={pda.status!}
+        />
+      ))}
+    </PDAsListContainer>
+  );
 }
