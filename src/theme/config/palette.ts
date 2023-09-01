13152fdf15ced58ec7b73112c0a4aa20cca540c0
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-01 16:14:24 -0300

    feat: dark theme landing

--- src/theme/config/palette.ts
+++ src/theme/config/palette.ts
@@ -2,7 +2,7 @@ import { PaletteOptions } from '@mui/material';
 
 import { brandColors } from './brand';
 
-export const palette: PaletteOptions = {
+export const lightPalette: PaletteOptions = {
   mode: 'light',
   primary: {
     main: brandColors.primary,
@@ -25,5 +25,30 @@ export const palette: PaletteOptions = {
   background: {
     default: '#F6F4FA',
     paper: '#fff',
+  },
+};
+
+export const darkPalette: PaletteOptions = {
+  mode: 'dark',
+  primary: {
+    main: brandColors.primary,
+  },
+  secondary: {
+    main: brandColors.secondary,
+  },
+  error: {
+    main: brandColors.error,
+  },
+  warning: {
+    main: brandColors.warning,
+  },
+  info: {
+    main: brandColors.info,
+  },
+  success: {
+    main: brandColors.success,
+  },
+  background: {
+    default: '#212121',
   },
 };
