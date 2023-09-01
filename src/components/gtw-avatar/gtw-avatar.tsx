7810447f577a611cb46176290350e19e2bf7a5cd
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-01 14:59:05 -0300

    feat: gtw avatar

---
+++ src/components/gtw-avatar/gtw-avatar.tsx
@@ -0,0 +1,29 @@
+import BoringAvatar from "boring-avatars";
+
+import { Avatar } from "@mui/material";
+
+type Props = {
+  src: string | undefined | null;
+  name: string;
+  alt?: string;
+  size?: number;
+  hasBorder?: boolean;
+}
+
+export default function GTWAvatar({ src, name, alt, hasBorder, size }: Props) {
+  if (!src) {
+    return <BoringAvatar variant="marble" name={name} size={size} />
+  }
+
+  return <Avatar src={src} alt={alt} sx={{
+    ... (hasBorder && {
+      borderWidth: 1,
+      borderStyle: "solid",
+      borderColor: "divider"
+    }),
+    ...(size && {
+      width: size,
+      height: size
+    })
+  }} />
+}
