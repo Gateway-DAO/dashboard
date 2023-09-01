f4dcd25461f2fe8e2cbabac85716f4ad3e419cf4
João Conserva <joao@mygateway.xyz>
2023-09-01 16:08:56 -0300

    chore: added width centered

--- src/theme/config/style-tokens.ts
+++ src/theme/config/style-tokens.ts
@@ -1,10 +1,20 @@
+import { SxProps } from '@mui/system';
+
 import { BreakpointsValue } from '../types';
 
 /**
  * Reusable style tokens for responsiveness acorss the website.
  */
 
-type BreakpointsValues = Required<Pick<BreakpointsValue<number>, 'xs' | 'md' | 'lg'>>;
+type BreakpointsValues = Required<
+  Pick<BreakpointsValue<number>, 'xs' | 'md' | 'lg'>
+>;
+
+export const WIDTH_CENTERED: SxProps = {
+  maxWidth: 550,
+  width: '100%',
+  mx: 'auto',
+};
 
 export const CONTAINER_PX: BreakpointsValues = {
   xs: 3,
