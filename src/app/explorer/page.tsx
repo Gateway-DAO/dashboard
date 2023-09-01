8e234972bdd6ed8ff6cb0a84dd404febc5e20b5f
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-01 15:06:28 -0300

    feat: moved explorer home

--- src/app/explorer/page.tsx
+++ src/app/explorer/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import routes from "@/constants/routes";
 
-export default function Home() {
+export default function ExplorerHome() {
 
   return <>
     <Link href={routes.dashboardUser}>
