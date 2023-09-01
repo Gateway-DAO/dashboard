341661bb07d5547d78707e940a227151f1d65493
João Conserva <joao@mygateway.xyz>
2023-09-01 09:51:15 -0300

    chore: updated skeleton

--- src/app/dashboard/user/asset/[id]/components/send-pda/send-pda-form-successfully-skeleton.tsx
+++ src/app/dashboard/user/asset/[id]/components/send-pda/send-pda-form-successfully-skeleton.tsx
@@ -11,16 +11,11 @@ export default function SendPdaFormSuccessSkeleton() {
           sx={{ borderRadius: '50%' }}
         />
       </Box>
-      <Skeleton
-        variant="text"
-        width={300}
-        height={80}
-        sx={{ mt: 3, mb: 1.5 }}
-      />
+      <Skeleton variant="text" width={300} height={80} sx={{ mt: 4, mb: 2 }} />
       <Skeleton
         variant="rectangular"
         width="100%"
-        height={100}
+        height={300}
         sx={{ borderRadius: 1, mb: 3 }}
       />
       <Skeleton variant="rounded" width="100%" height={40} sx={{ mb: 1.5 }} />
