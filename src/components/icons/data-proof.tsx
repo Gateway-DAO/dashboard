024199b9b8c6c369353611c9ed832d627201e78c
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-01 18:54:35 -0300

    feat: filled menu state

--- src/components/icons/data-proof.tsx
+++ src/components/icons/data-proof.tsx
@@ -1,24 +1,17 @@
-import { SvgIcon, SvgIconProps } from "@mui/material";
+import { SvgIcon, SvgIconProps } from '@mui/material';
 
 export default function DataProofIcon(props: SvgIconProps) {
   return (
     <SvgIcon
-      viewBox="0 0 40 40"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M5.33329 5.33301C3.86053 5.33301 2.66663 6.52692 2.66663 7.99967V10.3997H5.33329V7.99967L7.46663 7.99967V5.33301H5.33329Z" fill="black" fillOpacity="0.56" />
-      <path d="M10.3111 7.99967H14.5777V5.33301H10.3111V7.99967Z" fill="black" fillOpacity="0.56" />
-      <path d="M17.4222 7.99967H21.6888V5.33301H17.4222V7.99967Z" fill="black" fillOpacity="0.56" />
-      <path d="M24.5333 7.99967H26.6666V10.3997H29.3333V7.99967C29.3333 6.52691 28.1394 5.33301 26.6666 5.33301H24.5333V7.99967Z" fill="black" fillOpacity="0.56" />
-      <path d="M26.6666 13.5997V18.3997H29.3333V13.5997H26.6666Z" fill="black" fillOpacity="0.56" />
-      <path d="M5.33329 18.3997V13.5997H2.66663V18.3997H5.33329Z" fill="black" fillOpacity="0.56" />
-      <path d="M26.6666 21.5997V23.9997H24.5333V26.6663H26.6666C28.1394 26.6663 29.3333 25.4724 29.3333 23.9997V21.5997H26.6666Z" fill="black" fillOpacity="0.56" />
-      <path d="M5.33329 23.9997V21.5997H2.66663V23.9997C2.66663 25.4724 3.86053 26.6663 5.33329 26.6663H7.46663V23.9997H5.33329Z" fill="black" fillOpacity="0.56" />
-      <path d="M14.5777 23.9997H10.3111V26.6663H14.5777V23.9997Z" fill="black" fillOpacity="0.56" />
-      <path d="M21.6888 23.9997H17.4222V26.6663H21.6888V23.9997Z" fill="black" fillOpacity="0.56" />
-      <path d="M8.66663 9.33301C7.56206 9.33301 6.66663 10.2284 6.66663 11.333C6.66663 12.4376 7.56206 13.333 8.66663 13.333H16.6666C17.7712 13.333 18.6666 12.4376 18.6666 11.333C18.6666 10.2284 17.7712 9.33301 16.6666 9.33301H8.66663Z" fill="black" fillOpacity="0.56" />
+      <path
+        fill="currentColor"
+        d="M4 4a2 2 0 0 0-2 2v1.8h2V6h1.6V4H4ZM7.733 6h3.2V4h-3.2v2ZM13.067 6h3.2V4h-3.2v2ZM18.4 6H20v1.8h2V6a2 2 0 0 0-2-2h-1.6v2ZM20 10.2v3.6h2v-3.6h-2ZM4 13.8v-3.6H2v3.6h2ZM20 16.2V18h-1.6v2H20a2 2 0 0 0 2-2v-1.8h-2ZM4 18v-1.8H2V18a2 2 0 0 0 2 2h1.6v-2H4ZM10.933 18h-3.2v2h3.2v-2ZM16.267 18h-3.2v2h3.2v-2ZM6.5 7a1.5 1.5 0 1 0 0 3h6a1.5 1.5 0 0 0 0-3h-6Z"
+      />
     </SvgIcon>
-  )
+  );
 }
