74c24e6ee8bc9901a177ed9c59f0a07ec668abf9
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-04 16:03:05 -0300

    fix: typo

--- src/components/icons/data-outlined.tsx
+++ src/components/icons/data-outlined.tsx
@@ -11,7 +11,7 @@ export default function DataOutlinedIcon(props: SvgIconProps) {
       {...props}
     >
       <path fill="currentColor" d="M5 8.5A1.5 1.5 0 0 1 6.5 7h6a1.5 1.5 0 0 1 0 3h-6A1.5 1.5 0 0 1 5 8.5Z" />
-      <path fill="currentColor" fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm0 2h16v12H4V6Z" clip-rule="evenodd" />
+      <path fill="currentColor" fillRule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm0 2h16v12H4V6Z" clipRule="evenodd" />
 
     </SvgIcon>
   );
