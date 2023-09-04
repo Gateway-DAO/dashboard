ef6ed2d60809f888604c5cfde0226030e6a2d794
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-04 18:05:31 -0300

    feat: privateapi

--- src/hooks/use-private-api.ts
+++ src/hooks/use-private-api.ts
@@ -3,9 +3,6 @@ import { useSession } from 'next-auth/react';
 import { api } from '@/services/protocol/api';
 
 export default function usePrivateApi() {
-  const { data, status } = useSession();
-  if (status === 'authenticated') {
-    return api(data.token);
-  }
-  return null;
+  const { data } = useSession();
+  return api(data?.token ?? '');
 }
