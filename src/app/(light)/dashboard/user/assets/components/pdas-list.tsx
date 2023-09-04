b78557882a5d7762d4bd811f23279de2c8e396fe
André Felizardo <me@andrefelizardo.com.br>
2023-09-04 18:08:25 -0300

    chore: merge

--- src/app/(light)/dashboard/user/assets/components/pdas-list.tsx
+++ src/app/(light)/dashboard/user/assets/components/pdas-list.tsx
@@ -1,22 +1,76 @@
+'use client';
+
 import PdaCard from '@/components/pda-card/pda-card';
 import routes from '@/constants/routes';
+import usePrivateApi from '@/hooks/use-private-api';
 import { pdas as pdasLocales } from '@/locale/en/pda';
-import {
-  CredentialStatus,
-  Received_PdasQuery,
-} from '@/services/protocol/types';
+import { CredentialStatus, PrivateDataAsset } from '@/services/protocol/types';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { DeepPartial } from 'react-hook-form';
 
 import { Typography } from '@mui/material';
 
 import PDAsListContainer from './pdas-list-container';
 
 type Props = {
-  pdas: Received_PdasQuery['myPDAs'];
+  pdas: DeepPartial<PrivateDataAsset>[];
 };
 
-export default async function PDAsList({ pdas = [] }: Props) {
-  //TODO: Do pagination
-  if (!pdas.length) {
+// export default async function PDAsList({ pdas = [] }: Props) {
+//   //TODO: Do pagination
+//   if (!pdas.length) {
+//   pdas: DeepPartial<PrivateDataAsset>[];
+//   queryFn: (
+//     context: QueryFunctionContext<any>,
+//     pageSize: number
+//   ) => Promise<any[]>;
+// };
+
+export default function PDAsList({ pdas }: Props) {
+  const privateApi = usePrivateApi();
+  const pageSize = 2;
+  const {
+    data: items,
+    isLoading,
+    isFetchingNextPage,
+  } = useInfiniteQuery(
+    ['myPdas'],
+    ({ pageParam }) =>
+      async () => {
+        const res = await privateApi.received_pdas({
+          take: pageSize,
+          skip: pageParam || 0,
+        });
+        return res.myPDAs;
+      },
+    {
+      getNextPageParam: (lastPage: any = [], pages) =>
+        lastPage.length < pageSize ? undefined : pages.length * pageSize,
+      initialData: () => {
+        if (pdas && pdas.length > 0) {
+          return {
+            pageParams: [undefined],
+            pages: [pdas],
+          };
+        }
+      },
+    }
+  );
+  // const {
+  //   data: credentials, //[ ] Rename this data credentials
+  //   isLoading,
+  //   isFetchingNextPage,
+  //   fetchNextPage,
+  // } = useInfiniteQuery(
+  //   [queryString, dataModelId],
+  //   (options) => queryFn(options, pageSize),
+  //   {
+  //     getNextPageParam: (lastPage: any = [], pages) =>
+  //       lastPage.length < pageSize ? undefined : pages.length * pageSize,
+  //   }
+  // );
+
+  if (!items || !items.pages.length || !items.pages[0].length) {
     return (
       <Typography
         variant="body1"
--- src/app/(light)/dashboard/user/assets/received/page.tsx
+++ src/app/(light)/dashboard/user/assets/received/page.tsx
@@ -1,4 +1,7 @@
 import { getApiPrivate } from '@/services/protocol/api';
+// import { PrivateDataAsset } from '@/services/protocol/types';
+// import { QueryFunctionContext } from '@tanstack/react-query';
+// import { PartialDeep } from 'type-fest';
 
 import PDAsList from '../components/pdas-list';
 
@@ -8,7 +11,7 @@ export default async function DataAssetsPage() {
     return null;
   }
 
-  const pdas = (await apiPrivate.received_pdas({ take: 6, skip: 0 }))?.myPDAs;
+  const pdas = (await apiPrivate.received_pdas({ take: 2, skip: 0 }))?.myPDAs;
 
   return <PDAsList pdas={pdas} />;
 }
