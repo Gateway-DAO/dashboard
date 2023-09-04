b34366a75e0bf8c0cc62c346a67ccd3a7d5f47fe
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-04 15:45:22 -0300

    feat: org menu

--- src/app/(light)/dashboard/org/components/dashboard-org-bottom-bar.tsx
+++ src/app/(light)/dashboard/org/components/dashboard-org-bottom-bar.tsx
@@ -1,16 +1,14 @@
 "use client";
-import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useMemo } from "react";
 
+import useOrganization from "@/hooks/use-organization";
 
 import MenuBottomListItems from "../../components/menu-bottom-list-items";
 import dashboardOrgMenuItems from "./dashboard-org-menu-items";
 
-
 export default function DashboardOrgBottomBar() {
-  const bottomItems = [dashboardOrgMenuItems[0], dashboardOrgMenuItems[3]];
-  const innerMenuItems = dashboardOrgMenuItems.filter(item => !bottomItems.some(i => i.href === item.href));
-  const activePath = usePathname();
-
-  return <MenuBottomListItems activePath={activePath} bottomItems={bottomItems} menuItems={innerMenuItems} />
+  const { pathnameOrg } = useOrganization();
+  const menuItems = useMemo(() => pathnameOrg ? dashboardOrgMenuItems(pathnameOrg) : undefined, [pathnameOrg]);
+  if (!menuItems) return null;
+  return <MenuBottomListItems menuItems={menuItems} />
 }
--- src/app/(light)/dashboard/org/components/dashboard-org-menu-items.tsx
+++ src/app/(light)/dashboard/org/components/dashboard-org-menu-items.tsx
@@ -1,56 +1,33 @@
-
+import { GTWMenuItemProps } from '@/app/(light)/dashboard/components/menu-item/menu-item';
 import DataFilledIcon from '@/components/icons/data-filled';
+import DataOutlinedIcon from '@/components/icons/data-outlined';
+import DataProofFilledIcon from '@/components/icons/data-proof-filled';
+import DataProofOutlinedIcon from '@/components/icons/data-proof-outlined';
+import DataRequestFilledIcon from '@/components/icons/data-request-filled';
 import DataRequestOutlinedIcon from '@/components/icons/data-request-outlined';
-
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-
-import { GTWMenuItemProps } from '../../components/menu-item/menu-item';
+import routes from '@/constants/routes';
 
 /**
  * List all menu items of the user dashboard
  */
-export const dashboardOrgMenuItems: GTWMenuItemProps[] = [
-  {
-    name: 'Home',
-    href: '/',
-    icon: HomeOutlinedIcon,
-  },
+export const dashboardOrgMenuItems = (organization: string): GTWMenuItemProps[] => [
   {
     name: 'Issued data assets',
-    href: '/issued',
-    icon: DataRequestOutlinedIcon,
+    href: routes.dashboardOrgIssuedAssets.replace("[id]", organization),
+    icon: DataOutlinedIcon,
+    activeIcon: DataFilledIcon,
   },
   {
     name: 'Data requests',
-    href: '/user/data-requests',
+    href: routes.dashboardOrgRequests.replace("[id]", organization),
     icon: DataRequestOutlinedIcon,
+    activeIcon: DataRequestFilledIcon,
   },
   {
-    name: 'Org data assets',
-    href: '/dashboard',
-    icon: DataFilledIcon,
-  },
-  {
-    name: 'Data proofs',
-    href: '/dashboard',
-    icon: DataFilledIcon,
-  },
-  {
-    name: 'Saved and created data models',
-    href: '/dashboard',
-    icon: DataFilledIcon,
-  },
-  {
-    name: 'Activity',
-    href: '/activity',
-    icon: AccessTimeIcon,
-  },
-  {
-    name: 'Notifications',
-    href: '/notifications',
-    icon: NotificationsNoneIcon,
+    name: 'Received data proofs',
+    href: routes.dashboardOrgReceivedProofs.replace("[id]", organization),
+    icon: DataProofOutlinedIcon,
+    activeIcon: DataProofFilledIcon,
   },
 ];
 
