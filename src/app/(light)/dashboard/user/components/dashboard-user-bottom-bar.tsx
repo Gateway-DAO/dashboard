f6903d38a4ee2de07f822942546db7571dcadbb6
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-04 12:09:44 -0300

    feat: user menu

--- src/app/(light)/dashboard/user/components/dashboard-user-bottom-bar.tsx
+++ src/app/(light)/dashboard/user/components/dashboard-user-bottom-bar.tsx
@@ -1,16 +1,10 @@
 "use client";
-import { usePathname } from "next/navigation";
 import React from "react";
-
 
 import MenuBottomListItems from "../../components/menu-bottom-list-items";
 import dashboardUserMenuItems from "./dashboard-user-menu-items";
 
 
 export default function DashboardUserBottomBar() {
-  const bottomItems = [dashboardUserMenuItems[0], dashboardUserMenuItems[3]];
-  const innerMenuItems = dashboardUserMenuItems.filter(item => !bottomItems.some(i => i.href === item.href));
-  const activePath = usePathname();
-
-  return <MenuBottomListItems activePath={activePath} bottomItems={bottomItems} menuItems={innerMenuItems} />
+  return <MenuBottomListItems menuItems={dashboardUserMenuItems} />
 }
--- src/app/(light)/dashboard/user/components/dashboard-user-menu-items.tsx
+++ src/app/(light)/dashboard/user/components/dashboard-user-menu-items.tsx
@@ -1,31 +1,27 @@
 import { GTWMenuItemProps } from '@/app/(light)/dashboard/components/menu-item/menu-item';
 import DataFilledIcon from '@/components/icons/data-filled';
+import DataOutlinedIcon from '@/components/icons/data-outlined';
 import DataProofFilledIcon from '@/components/icons/data-proof-filled';
 import DataProofOutlinedIcon from '@/components/icons/data-proof-outlined';
+import DataRequestFilledIcon from '@/components/icons/data-request-filled';
 import DataRequestOutlinedIcon from '@/components/icons/data-request-outlined';
 import routes from '@/constants/routes';
-
-// import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
 /**
  * List all menu items of the user dashboard
  */
 export const dashboardUserMenuItems: GTWMenuItemProps[] = [
   {
-    name: 'Home',
-    href: routes.dashboardUser,
-    icon: HomeOutlinedIcon,
-  },
-  {
     name: 'Data assets',
     href: routes.dashboardUserReceivedAssets,
-    icon: DataFilledIcon,
+    icon: DataOutlinedIcon,
+    activeIcon: DataFilledIcon,
   },
   {
     name: 'Data requests',
     href: routes.dashboardUserRequests,
     icon: DataRequestOutlinedIcon,
+    activeIcon: DataRequestFilledIcon,
   },
   {
     name: 'Data proofs',
