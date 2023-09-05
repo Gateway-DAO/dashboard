095a48fa1b432573bea2877294b1787f20c21752
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-05 15:26:00 -0300

    feat: outlined button colors

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -2,10 +2,25 @@ import { Theme } from '@mui/material/styles';
 
 export const MuiButton: Extract<Theme['components'], object>['MuiButton'] = {
   styleOverrides: {
-    root: ({theme}) => ({
+    root: ({ theme }) => ({
       textTransform: 'unset',
       borderRadius: theme.spacing(5),
       fontWeight: 'bold',
+    }),
+    outlinedError: ({ theme }) => ({
+      color: theme.palette.error.dark,
+    }),
+    outlinedInfo: ({ theme }) => ({
+      color: theme.palette.info.dark,
+    }),
+    outlinedWarning: ({ theme }) => ({
+      color: theme.palette.warning.dark,
+    }),
+    outlinedSecondary: ({ theme }) => ({
+      color: theme.palette.secondary.dark,
+    }),
+    outlinedSuccess: ({ theme }) => ({
+      color: theme.palette.success.dark,
     }),
   },
 };
