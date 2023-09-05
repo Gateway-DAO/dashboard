b24798cd578cd1acef32d8b42fbf209532b2301e
André Felizardo <me@andrefelizardo.com.br>
2023-09-05 16:39:18 -0300

    fix: empty state

---
+++ src/app/(light)/dashboard/user/assets/components/sent-pdas-table.tsx
@@ -0,0 +1,10 @@
+import { PrivateDataAsset } from '@/services/protocol/types';
+import { DeepPartial } from 'react-hook-form';
+
+type Props = {
+  pdas: DeepPartial<PrivateDataAsset>[];
+};
+
+export default function SentPdasTable({ pdas }: Props) {
+  return <>bora</>;
+}
