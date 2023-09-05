737a09a9935c6cde7c36e1ff0b0674d0fc6f68af
João Conserva <joao@mygateway.xyz>
2023-09-05 15:34:12 -0300

    chore: fixed proof types

--- src/app/(light)/dashboard/user/proof/[id]/pda/[pdaId]/page.tsx
+++ src/app/(light)/dashboard/user/proof/[id]/pda/[pdaId]/page.tsx
@@ -2,10 +2,11 @@ import { Metadata } from 'next';
 
 import PDAItem from '@/app/(light)/dashboard/user/asset/[id]/components/pda-item';
 import { apiPublic } from '@/services/protocol/api';
+import { PdaQuery } from '@/services/protocol/types';
 
-const getPDA = async (id: string) => {
+const getPDA = async (id: string): Promise<PdaQuery['PDAbyId'] | null> => {
   const pda = await apiPublic.pda({ id });
-  return pda?.PDAbyId?.dataAsset;
+  return pda?.PDAbyId;
 };
 
 export async function generateMetadata({
@@ -15,8 +16,8 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   const pda = await getPDA(params.pdaId);
   return {
-    title: `${pda?.title} PDA - Gateway Network`,
-    description: pda?.description,
+    title: `${pda?.dataAsset?.title} PDA - Gateway Network`,
+    description: pda?.dataAsset?.description,
   };
 }
 
