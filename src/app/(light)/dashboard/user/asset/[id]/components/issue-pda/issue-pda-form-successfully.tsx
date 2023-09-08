255e6f047132aa07fe5dff2a4ce53ae527b0e128
João Conserva <joao@mygateway.xyz>
2023-09-08 18:22:29 -0300

    chore: updated proof successfully modal

--- src/app/(light)/dashboard/user/asset/[id]/components/issue-pda/issue-pda-form-successfully.tsx
+++ src/app/(light)/dashboard/user/asset/[id]/components/issue-pda/issue-pda-form-successfully.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { useEffect, useState } from 'react';
+
 import ProofCardInfo from '@/app/(light)/dashboard/user/proof/[id]/components/proof-card-info';
 import ProofCardTitle from '@/app/(light)/dashboard/user/proof/[id]/components/proof-card-title';
 import { common } from '@/locale/en/common';
@@ -7,11 +10,23 @@ import { CheckOutlined } from '@mui/icons-material';
 import LinkIcon from '@mui/icons-material/Link';
 import { Avatar, Box, Button, Stack, Typography } from '@mui/material';
 
+import IssuePdaFormSuccessSkeleton from './issue-pda-form-successfully-skeleton';
+
 type Props = {
   id: string;
 };
 
 export default function IssuePdaFormSuccessfully({ id }: Props) {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // TODO: Remove MOCK
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   // TODO: Remove MOCK
   const proof = {
     id,
@@ -34,30 +49,40 @@ export default function IssuePdaFormSuccessfully({ id }: Props) {
   };
 
   return (
-    <Stack>
-      <Box sx={{ position: 'absolute', top: { xs: 24, md: 48 } }}>
-        <Avatar
-          sx={{ backgroundColor: 'success.main', color: 'action.active' }}
-        >
-          <CheckOutlined />
-        </Avatar>
-      </Box>
-      <Typography fontSize={34} sx={{ mb: 6 }}>
-        {pda.share.successfully_title}
-      </Typography>
-      <ProofCardTitle proof={proof} />
-      <ProofCardInfo proof={proof} />
-      <Button
-        variant="contained"
-        id="share-pda-button-check-now"
-        sx={{ mb: 1.5 }}
-      >
-        {common.actions.check_now}
-      </Button>
-      <Button variant="outlined" id="share-pda-button-copy-url" sx={{ mb: 3 }}>
-        <LinkIcon sx={{ mr: 1 }} />
-        {common.actions.copy_url}
-      </Button>
-    </Stack>
+    <>
+      {loading ? (
+        <IssuePdaFormSuccessSkeleton />
+      ) : (
+        <Stack>
+          <Box sx={{ position: 'absolute', top: { xs: 24, md: 48 } }}>
+            <Avatar
+              sx={{ backgroundColor: 'success.main', color: 'action.active' }}
+            >
+              <CheckOutlined />
+            </Avatar>
+          </Box>
+          <Typography fontSize={34} sx={{ mb: 6 }}>
+            {pda.share.successfully_title}
+          </Typography>
+          <ProofCardTitle proof={proof} />
+          <ProofCardInfo proof={proof} />
+          <Button
+            variant="contained"
+            id="share-pda-button-check-now"
+            sx={{ mb: 1.5 }}
+          >
+            {common.actions.check_now}
+          </Button>
+          <Button
+            variant="outlined"
+            id="share-pda-button-copy-url"
+            sx={{ mb: 3 }}
+          >
+            <LinkIcon sx={{ mr: 1 }} />
+            {common.actions.copy_url}
+          </Button>
+        </Stack>
+      )}
+    </>
   );
 }
