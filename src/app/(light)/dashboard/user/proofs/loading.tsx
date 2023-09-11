a3b496339304a9887b46e07709a364900f7e4c6f
João Conserva <joao@mygateway.xyz>
2023-09-11 18:05:38 -0300

    chore: updated loading

--- src/app/(light)/dashboard/user/proofs/loading.tsx
+++ src/app/(light)/dashboard/user/proofs/loading.tsx
@@ -2,26 +2,23 @@ import { Skeleton, Stack } from '@mui/material';
 
 export default function DataAssetsLoadingPage() {
   return (
-    <Stack gap={1}>
-      <Stack direction="row" gap={2}>
-        <Stack sx={{ flexBasis: '100%' }}>
+    <Stack gap={1} sx={{ pt: 5 }}>
+      <Stack direction="row" gap={2} sx={{ mb: 3 }}>
+        <Stack sx={{ flexGrow: 1 }}>
+          <Skeleton variant="text" height={40} width="20%" />
+        </Stack>
+        <Stack sx={{ flexBasis: '20%' }}>
           <Skeleton variant="text" height={40} width="50%" />
         </Stack>
-        <Stack sx={{ flexBasis: '100%' }}>
-          <Skeleton variant="text" height={40} width="50%" />
-        </Stack>
-        <Stack sx={{ flexBasis: '100%' }}>
-          <Skeleton variant="text" height={40} width="50%" />
-        </Stack>
-        <Stack sx={{ flexBasis: '100%' }}>
+        <Stack sx={{ flexBasis: '20%' }}>
           <Skeleton variant="text" height={40} width="50%" />
         </Stack>
       </Stack>
-      <Stack gap={1}>
-        <Skeleton variant="text" height={40} />
-        <Skeleton variant="text" height={40} />
-        <Skeleton variant="text" height={40} />
-        <Skeleton variant="text" height={40} />
+      <Stack gap={2}>
+        <Skeleton variant="text" height={60} />
+        <Skeleton variant="text" height={60} />
+        <Skeleton variant="text" height={60} />
+        <Skeleton variant="text" height={60} />
       </Stack>
     </Stack>
   );
