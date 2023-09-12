9824d582ac338975fec8b440cd7515111ea2a202
João Conserva <joao@mygateway.xyz>
2023-09-12 17:23:26 -0300

    chore: updated proofs page

--- src/app/(light)/dashboard/user/proofs/components/table-shared.tsx
+++ src/app/(light)/dashboard/user/proofs/components/table-shared.tsx
@@ -1,9 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 
 import GTWAvatar from '@/components/gtw-avatar/gtw-avatar';
-import Loading from '@/components/loadings/loading';
 import routes from '@/constants/routes';
 import { Proof } from '@/services/protocol/types';
 import {
@@ -22,63 +20,53 @@ type Props = {
 };
 
 export function TableSharedDataProofs({ proofs }: Props) {
-  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
   return (
     <TableSharedDataProofsContainer>
-      {isLoading ? (
-        <Loading size={16} />
-      ) : (
-        <>
-          {proofs?.map((proof) => (
-            <TableRow
-              sx={{
-                '&:last-child td, ˆ:last-child th': { border: 0 },
-                '& td:first-child': {
-                  pl: CONTAINER_PX,
-                },
-                '& td:last-child': {
-                  pr: CONTAINER_PX,
-                },
-                display: 'grid',
-                gridTemplateColumns: '3fr 1fr 1fr',
-                mx: NEGATIVE_CONTAINER_PX,
-                cursor: 'pointer',
-              }}
-              hover={true}
-              key={proof?.id}
-              onClick={() => {
-                setIsLoading(true);
-                router.push(routes.dashboardUserProof(proof?.id));
-              }}
-            >
-              <TableCell
-                sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  gap: 2,
-                }}
-              >
-                <GTWAvatar name={''} />
-                <Typography variant="subtitle1">
-                  {proof?.owner?.user?.gatewayId}
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="body1">
-                  {dayjs(proof?.createdAt).format('MM/DD/YYYY, h:mm A')}
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="body2">
-                  {proof?.data?.PDAs?.length}
-                </Typography>
-              </TableCell>
-            </TableRow>
-          ))}
-        </>
-      )}
+      {proofs?.map((proof) => (
+        <TableRow
+          sx={{
+            '&:last-child td, ˆ:last-child th': { border: 0 },
+            '& td:first-child': {
+              pl: CONTAINER_PX,
+            },
+            '& td:last-child': {
+              pr: CONTAINER_PX,
+            },
+            display: 'grid',
+            gridTemplateColumns: '3fr 1fr 1fr',
+            mx: NEGATIVE_CONTAINER_PX,
+            cursor: 'pointer',
+          }}
+          hover={true}
+          key={proof?.id}
+          onClick={() => {
+            router.push(routes.dashboardUserProof(proof?.id));
+          }}
+        >
+          <TableCell
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 2,
+            }}
+          >
+            <GTWAvatar name={''} />
+            <Typography variant="subtitle1">
+              {proof?.owner?.user?.gatewayId}
+            </Typography>
+          </TableCell>
+          <TableCell>
+            <Typography variant="body1">
+              {dayjs(proof?.createdAt).format('MM/DD/YYYY, h:mm A')}
+            </Typography>
+          </TableCell>
+          <TableCell>
+            <Typography variant="body2">{proof?.data?.PDAs?.length}</Typography>
+          </TableCell>
+        </TableRow>
+      ))}
     </TableSharedDataProofsContainer>
   );
 }
