3823c926c88c95009ff5544cc246e740d050dce2
João Conserva <joao@mygateway.xyz>
2023-09-12 17:13:00 -0300

    chore: created proof item page

--- src/app/(light)/dashboard/user/proof/[id]/components/proof-card-info.tsx
+++ src/app/(light)/dashboard/user/proof/[id]/components/proof-card-info.tsx
@@ -3,45 +3,64 @@ import { PDAStatusChip } from '@/components/pda-card/pda-status-chip';
 import { TableCellContainer } from '@/components/table-cell-container/table-cell-container';
 import { pda } from '@/locale/en/pda';
 import { proof as proofLocale } from '@/locale/en/proof';
+import {
+  CredentialStatus,
+  DataResourceStatus,
+  Proof,
+} from '@/services/protocol/types';
 import dayjs from 'dayjs';
+import { PartialDeep } from 'type-fest';
 
-import { Stack, Divider } from '@mui/material';
+import { Stack, Divider, Card } from '@mui/material';
 
 type Props = {
-  proof: any; // TODO: Add type
+  proof: PartialDeep<Proof>;
 };
 
 export default function ProofCardInfo({ proof }: Props) {
   return (
     <Stack
+      component={Card}
+      variant="outlined"
       sx={{
-        border: '1px solid',
-        borderColor: 'divider',
-        borderRadius: 1,
         mb: 3,
-        backgroundColor: 'common.white',
       }}
       divider={<Divider sx={{ width: '100%' }} />}
     >
       <TableCellContainer>
         <CardCell label={proofLocale.share_date}>
-          {dayjs(proof?.issuance_date).format('MM/DD/YYYY, h:mm A')}
+          {dayjs(proof?.createdAt).format('MM/DD/YYYY, h:mm A')}
         </CardCell>
-        {!isNaN(proof?.sharing_cost) && (
-          <CardCell label={pda.share.sharing_cost}>
-            {`${(proof?.sharing_cost ?? 0).toLocaleString('en-US', {
-              style: 'currency',
-              currency: 'USD',
-              currencyDisplay: 'symbol',
-            })}`}
-          </CardCell>
-        )}
+        {!isNaN(proof?.facilitationFee as number) &&
+          proof?.facilitationFee !== 0 && (
+            <CardCell label={pda.share.sharing_cost}>
+              {`${(proof?.facilitationFee ?? 0).toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                currencyDisplay: 'symbol',
+              })}`}
+            </CardCell>
+          )}
       </TableCellContainer>
       <TableCellContainer>
         <CardCell label={proofLocale.status.title}>
-          <PDAStatusChip status={proof.status} isProof={true} size="small" />
+          <PDAStatusChip
+            status={CredentialStatus.Valid}
+            isProof={true}
+            size="small"
+          />
         </CardCell>
       </TableCellContainer>
+      {proof?.dataRequest?.status === DataResourceStatus.Accepted && (
+        <TableCellContainer>
+          <CardCell label={proofLocale.request_id}>
+            {proof?.dataRequest?.id}
+          </CardCell>
+          <CardCell label={proofLocale.request_template_id}>
+            {proof?.dataRequest?.dataRequestTemplate?.id}
+          </CardCell>
+        </TableCellContainer>
+      )}
     </Stack>
   );
 }
