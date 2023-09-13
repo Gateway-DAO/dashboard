09ce30de34d87812616a065a93b6db35a1e8b42b
João Conserva <joao@mygateway.xyz>
2023-09-13 11:58:43 -0300

    chore: created loading table

--- src/app/(light)/dashboard/user/proofs/loading.tsx
+++ src/app/(light)/dashboard/user/proofs/loading.tsx
@@ -1,25 +1,5 @@
-import { Skeleton, Stack } from '@mui/material';
+import LoadingTable from '@/components/loadings/loading-table';
 
 export default function DataProofsLoadingPage() {
-  return (
-    <Stack gap={1} sx={{ pt: 5 }}>
-      <Stack direction="row" gap={2} sx={{ mb: 2 }}>
-        <Stack sx={{ flexGrow: 1 }}>
-          <Skeleton variant="text" height={40} width="20%" />
-        </Stack>
-        <Stack sx={{ flexBasis: '20%' }}>
-          <Skeleton variant="text" height={40} width="50%" />
-        </Stack>
-        <Stack sx={{ flexBasis: '20%' }}>
-          <Skeleton variant="text" height={40} width="50%" />
-        </Stack>
-      </Stack>
-      <Stack gap={1}>
-        <Skeleton variant="text" height={50} />
-        <Skeleton variant="text" height={50} />
-        <Skeleton variant="text" height={50} />
-        <Skeleton variant="text" height={50} />
-      </Stack>
-    </Stack>
-  );
+  return <LoadingTable />;
 }
---
+++ src/components/loadings/loading-table.tsx
@@ -0,0 +1,31 @@
+import { Skeleton, Stack } from '@mui/material';
+
+type Props = {
+  columns?: number;
+};
+
+export default function LoadingTable({ columns = 5 }: Props) {
+  const arrayColumns = Array.from({ length: columns }, () => 1);
+
+  return (
+    <Stack gap={1} sx={{ pt: 3 }}>
+      <Stack direction="row" gap={2} sx={{ mb: 2 }}>
+        {arrayColumns.map((item) => (
+          <Stack key={item} sx={{ flexGrow: 1 }}>
+            <Skeleton
+              variant="text"
+              height={30}
+              width={100}
+              sx={{ maxWidth: '50%' }}
+            />
+          </Stack>
+        ))}
+      </Stack>
+      <Stack gap={1}>
+        <Skeleton variant="text" height={40} />
+        <Skeleton variant="text" height={40} />
+        <Skeleton variant="text" height={40} />
+      </Stack>
+    </Stack>
+  );
+}
