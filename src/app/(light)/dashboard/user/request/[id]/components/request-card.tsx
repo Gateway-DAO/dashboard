685d8fd46f58ccda3c8e934e85493eb7c7adc365
Lucas @kbooz <lucas1inacio7souza@gmail.com>
2023-09-13 11:16:19 -0300

    feat: request and i18n

--- src/app/(light)/dashboard/user/request/[id]/components/request-card.tsx
+++ src/app/(light)/dashboard/user/request/[id]/components/request-card.tsx
@@ -1,29 +1,66 @@
 "use client";
 
+import Link from "next/link";
+
 import GTWAvatar from "@/components/gtw-avatar/gtw-avatar";
+import routes from "@/constants/routes";
+import { common } from "@/locale/en/common";
+import { request } from "@/locale/en/request";
+import { DataResourceStatus } from "@/services/protocol/types";
 
 import { Box, Button, Divider, Stack, Typography, alpha } from "@mui/material";
 
-export default function RequestCard() {
+type Props = {
+  requester: string,
+  status: DataResourceStatus,
+  proofId?: string
+}
+
+export default function RequestCard({
+  requester,
+  status,
+  proofId
+}: Props) {
+
   return (
     <Box sx={{
-      backgroundColor: (theme) => alpha(theme.palette.primary.main, theme.palette.action.focusOpacity),
+      backgroundColor: (theme) => {
+        const color = status === DataResourceStatus.Rejected ? theme.palette.error.main : theme.palette.primary.main;
+        const focusOpacity = status === DataResourceStatus.Rejected ? theme.palette.action.disabledOpacity : theme.palette.action.focusOpacity;
+        return alpha(color, focusOpacity)
+      },
       borderRadius: 1
     }}>
       <Box sx={{ p: 2 }}>
-        <Typography component="p" variant="caption" color="text.secondary" sx={{ mb: 2 }}>Requested by</Typography>
+        <Typography component="p" variant="caption" color="text.secondary" sx={{ mb: 2 }}>{request.request_card.title}</Typography>
         <Stack direction="row" alignItems="center" gap={2}>
-          <GTWAvatar name="chase" /> <Typography variant="h5">Chase</Typography>
+          <GTWAvatar name="chase" /> <Typography variant="h5">{requester}</Typography>
         </Stack>
       </Box>
       <Divider />
       <Box sx={{ p: 2 }}>
-        <Typography variant="h5">{"You're ready to proceed"}</Typography>
-        <Typography >Chase is requesting to access your private data assets</Typography>
-        <Stack direction="row" gap={1} sx={{ mt: 3 }}>
-          <Button variant="contained" color="primary">Authorize</Button>
-          <Button variant="outlined" color="primary">Reject</Button>
-        </Stack>
+        {status === DataResourceStatus.Pending && (<>
+          <Typography variant="h5">{request.request_card.content.pending.title}</Typography>
+          <Typography >{request.request_card.content.pending.description(requester)}</Typography>
+          <Stack direction="row" gap={1} sx={{ mt: 3 }}>
+            <Button variant="contained" color="primary">{common.actions.accept}</Button>
+            <Button variant="outlined" color="primary">{common.actions.reject}</Button>
+          </Stack>
+        </>)}
+        {status === DataResourceStatus.Rejected && (<>
+          <Typography variant="h5">{request.request_card.content.rejected.title}</Typography>
+          <Typography >{request.request_card.content.rejected.description(requester)}</Typography>
+          <Stack direction="row" gap={1} sx={{ mt: 3 }}>
+            <Button variant="outlined" color="primary">{common.actions.learn_more}</Button>
+          </Stack>
+        </>)}
+        {status === DataResourceStatus.Accepted && (<>
+          <Typography variant="h5">{request.request_card.content.accepted.title}</Typography>
+          <Typography >{request.request_card.content.accepted.description(requester)}</Typography>
+          <Stack direction="row" gap={1} sx={{ mt: 3 }}>
+            <Button component={Link} href={routes.dashboardUserProof(proofId ?? "")} variant="contained" color="primary">{common.actions.check_data_proof}</Button>
+          </Stack>
+        </>)}
       </Box>
     </Box>
   );
