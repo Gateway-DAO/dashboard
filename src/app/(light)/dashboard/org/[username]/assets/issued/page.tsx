c18985f90073eb9b51e6a7efacc53de9b9beeef6
andrefelizardo <andreluis.felizardo@outlook.com>
2023-09-14 14:41:42 -0300

    feat: create reusable configuration for grids

--- src/app/(light)/dashboard/org/[username]/assets/issued/page.tsx
+++ src/app/(light)/dashboard/org/[username]/assets/issued/page.tsx
@@ -1,25 +1,19 @@
+import TitleLayout from '@/components/title-layout/title-layout';
 import { pdas } from '@/locale/en/pda';
 
-import { Box, Stack, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 import PDAsTable from './components/pdas-table';
 
 export default function OrganizationIssuedAssetsPage() {
   return (
-    <Stack>
-      <Box
-        sx={{
-          mb: {
-            xs: 4,
-            md: 5,
-            lg: 6,
-          },
-        }}
-      >
-        <Typography variant="h3">{pdas.my_data_assets}</Typography>
-        <Typography variant="body1">{pdas.data_assets_subtitle}</Typography>
-      </Box>
+    <Box sx={{ py: 2 }}>
+      <TitleLayout
+        title={pdas.my_data_assets}
+        subtitle={pdas.data_assets_subtitle}
+        titleId="title-org-assets"
+      />
       <PDAsTable />
-    </Stack>
+    </Box>
   );
 }
