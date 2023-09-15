bd602056d35664e97707da30b1bb2cff1cc9c540
Lucas @kbooz <lucas1inacio7souza@gmail.com>
2023-09-15 16:36:30 -0300

    feat: copy button

--- src/locale/en/common.ts
+++ src/locale/en/common.ts
@@ -30,6 +30,7 @@ export const common = {
     show_activity: 'See Activity',
     learn_more: 'Learn more',
     check_now: 'Check now',
+    copy: 'Copy',
     copy_url: 'Copy URL',
     connect_now: 'Connect now',
     back_to_home: 'Back to home',
