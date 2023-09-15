671fb940a66870fd13e785f7621112a6624a3d0e
Lucas @kbooz <lucas1inacio7souza@gmail.com>
2023-09-15 18:46:23 -0300

    feat: developers section

---
+++ src/components/toggle-visibility-button/toggle-visibility-button.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ToggleVisibilityButton from './toggle-visibility-button';
+
+const meta: Meta<typeof ToggleVisibilityButton> = {
+  title: 'Components/Copy Button',
+  component: ToggleVisibilityButton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof ToggleVisibilityButton>;
+
+export const Playground: Story = {
+  args: {
+    isVisible: false,
+  },
+};
