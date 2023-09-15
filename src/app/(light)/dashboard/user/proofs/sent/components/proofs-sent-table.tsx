6a341a24fddbfc507aeb7255acf78bd7fc74b51d
andrefelizardo <andreluis.felizardo@outlook.com>
2023-09-15 19:33:02 -0300

    fix: new API verifier

--- src/app/(light)/dashboard/user/proofs/sent/components/proofs-sent-table.tsx
+++ src/app/(light)/dashboard/user/proofs/sent/components/proofs-sent-table.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 import {
   defaultGridConfiguration,
   defaultGridCustomization,
 } from '@/components/data-grid/grid-default';
 import GTWAvatar from '@/components/gtw-avatar/gtw-avatar';
+import { queries } from '@/constants/queries';
 import routes from '@/constants/routes';
+import { useSession } from '@/context/session-provider';
 import { proofs } from '@/locale/en/proof';
 import { Proof } from '@/services/protocol/types';
 import { limitCharsCentered } from '@/utils/string';
+import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
 import { PartialDeep } from 'type-fest';
 
@@ -76,16 +80,52 @@ const columns: GridColDef<PartialDeep<Proof>>[] = [
 
 type Props = {
   data: PartialDeep<Proof>[];
+  count: number;
 };
 
-export default function ProofsSentTable({ data }: Props) {
+export default function ProofsSentTable({
+  data: initialData,
+  count = 0,
+}: Props) {
   const router = useRouter();
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0,
+    pageSize: 5,
+  });
+
+  const { privateApi } = useSession();
+  const { data, isFetching } = useQuery({
+    // eslint-disable-next-line @tanstack/query/exhaustive-deps
+    queryKey: [
+      queries.proofs_sent,
+      paginationModel ? paginationModel.page : 0,
+      paginationModel ? paginationModel.pageSize : 5,
+    ],
+    queryFn: () =>
+      privateApi?.received_proofs({
+        skip: paginationModel.page * paginationModel.pageSize,
+        take: paginationModel.pageSize,
+      }),
+    select: (data: any) => data?.sentProofs,
+    initialData: initialData && initialData.length ? initialData : null,
+  });
+
+  const setNewPage = ({ page }: { page: number }) => {
+    setPaginationModel((prev) => ({
+      ...prev,
+      page: page ? page : 0,
+    }));
+  };
 
   return (
     <DataGrid
       {...defaultGridConfiguration}
-      rows={data}
+      rows={data && data.length ? data : initialData}
+      paginationModel={paginationModel}
+      loading={isFetching}
+      onPaginationModelChange={setNewPage}
       columns={columns}
+      rowCount={count}
       sx={defaultGridCustomization}
       onRowClick={(value) => {
         router.push(routes.dashboardUserProof(value?.id));
