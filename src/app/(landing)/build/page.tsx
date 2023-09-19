9f6be2af4db477e2257217b7f79de8ac78b91106
Gustavo Cardoso <g.cardoso23@outlook.com>
2023-09-19 11:24:26 -0300

    feat: landing - build page

--- src/app/(landing)/build/page.tsx
+++ src/app/(landing)/build/page.tsx
@@ -2,10 +2,66 @@
 
 import { HeaderContextProvider } from '@/app/(landing)/contexts/header-context';
 
+import Hero from '../build/components/hero';
+import Footer from '../components/footer';
+import Header from '../components/header';
+import TemplateDocsSection from './components/template-docs-section';
+
 export default function BuildPage() {
   return (
     <HeaderContextProvider>
-      <h1></h1>
+      <Header />
+      <Hero />
+      <TemplateDocsSection
+        title="Become an Issuer"
+        button={{
+          text: 'Open issuer docs',
+          href: '/',
+        }}
+        cards={[
+          {
+            title: 'Create Unique Data-Models',
+            description: 'Easy create Data models that fits your needs',
+            href: '/',
+          },
+          {
+            title: 'Automate Issuance',
+            description: 'SDK integration step-by-step',
+            href: '/',
+          },
+          {
+            title: 'Control PDA Status',
+            description: 'How to Upgrade, Revoke, suspend and Update PDAs',
+            href: '/',
+          },
+          {
+            title: 'Monetize Data-Sharing',
+            description: 'How to monetize using our Verification System',
+            href: '/',
+          },
+        ]}
+      />
+
+      <TemplateDocsSection
+        title="Become a Verifier"
+        button={{
+          text: 'Open verifier docs',
+          href: '/',
+        }}
+        cards={[
+          {
+            title: 'Creating a Data Request',
+            description: 'Easily create Data models that fits your needs',
+            href: '/',
+          },
+          {
+            title: 'Verification Widget Integration',
+            description: 'How to integrate our widget into your platform',
+            href: '/',
+          },
+        ]}
+      />
+      <Footer variant="dark" />
     </HeaderContextProvider>
   );
 }
