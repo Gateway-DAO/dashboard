8e6c3839c1903d565dfda7851110d282e157bd6f
João Conserva <joao@mygateway.xyz>
2023-09-19 10:35:33 -0300

    chore: added organization proofs

--- src/app/(light)/dashboard/org/[username]/proofs/received/page.tsx
+++ src/app/(light)/dashboard/org/[username]/proofs/received/page.tsx
@@ -1,3 +1,44 @@
-export default function OrganizationReceivedProofsPage() {
-  return <>OrganizationReceivedProofsPage</>;
+import TitleLayout from '@/components/title-layout/title-layout';
+import { proofs as proofsLocales } from '@/locale/en/proof';
+import { getPrivateApi } from '@/services/protocol/api';
+import { Proof } from '@/services/protocol/types';
+import { PartialDeep } from 'type-fest';
+
+import { Box, Typography } from '@mui/material';
+
+import ProofsReceivedTable from './components/org-proofs-received-table';
+
+export default async function OrganizationReceivedProofsPage() {
+  const privateApi = await getPrivateApi();
+
+  const proofs = (await privateApi.received_proofs({ take: 5, skip: 0 }))
+    ?.receivedProofs as PartialDeep<Proof>[];
+  const count = (await privateApi.countReceivedProofs()).receivedProofsCount;
+
+  return (
+    <Box sx={{ py: 2 }}>
+      <TitleLayout
+        title={proofsLocales.received_proofs}
+        subtitle={proofsLocales.received_proofs_subtitle}
+        titleId="title-org-proofs"
+      />
+
+      <Box sx={{ pt: 1 }}>
+        <>
+          {proofs && proofs.length > 0 && (
+            <ProofsReceivedTable data={proofs} count={count} />
+          )}
+          {proofs && proofs.length === 0 && (
+            <Typography
+              variant="body1"
+              color="text.secondary"
+              sx={{ textAlign: 'center', width: '100%' }}
+            >
+              {proofsLocales.empty}
+            </Typography>
+          )}
+        </>
+      </Box>
+    </Box>
+  );
 }
