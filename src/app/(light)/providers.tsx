4deba7fb33503b65816f792c4684c044094637fc
João Conserva <joao@mygateway.xyz>
2023-09-20 11:55:01 -0300

    chore: added progress bar

--- src/app/(light)/providers.tsx
+++ src/app/(light)/providers.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 import { PropsWithChildren, useState } from 'react';
 
 import Notistack from '@/components/notistack/notistack';
 import { queryClientConfig } from '@/services/query-client';
 import { ThemeProvider } from '@/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function Providers({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient(queryClientConfig));
@@ -18,6 +19,12 @@ export default function Providers({ children }: PropsWithChildren) {
         <ThemeProvider>
           <Notistack>
             <ReactQueryDevtools initialIsOpen={false} />
+            <ProgressBar
+              height="4px"
+              color="#70ECFE"
+              options={{ showSpinner: false }}
+              shallowRouting
+            />
             {children}
           </Notistack>
         </ThemeProvider>
