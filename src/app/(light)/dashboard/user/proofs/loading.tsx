cfe9792f06b32d162fd2a11db4b8a154e0052577
João Conserva <joao@mygateway.xyz>
2023-09-20 12:26:32 -0300

    chore: removed tables skeleton

--- src/app/(light)/dashboard/user/proofs/loading.tsx
+++
@@ -1,5 +0,0 @@
-import SkeletonTable from '@/components/loadings/skeleton-table/skeleton-table';
-
-export default function DataProofsLoadingPage() {
-  return <SkeletonTable columns={3} />;
-}
--- src/app/(light)/dashboard/user/requests/loading.tsx
+++
@@ -1,5 +0,0 @@
-import SkeletonTable from '@/components/loadings/skeleton-table/skeleton-table';
-
-export default function DataRequestsLoadingPage() {
-  return <SkeletonTable columns={5} />;
-}
