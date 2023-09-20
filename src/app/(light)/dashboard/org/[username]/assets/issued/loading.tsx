58eb06971d41e66261ca894b38cff746db6f86d6
João Conserva <joao@mygateway.xyz>
2023-09-20 12:26:32 -0300

    chore: removed tables skeleton

--- src/app/(light)/dashboard/org/[username]/assets/issued/loading.tsx
+++
@@ -1,5 +0,0 @@
-import SkeletonTable from '@/components/loadings/skeleton-table/skeleton-table';
-
-export default function OrgIssuedDataAssetsLoading() {
-  return <SkeletonTable columns={5} />;
-}
