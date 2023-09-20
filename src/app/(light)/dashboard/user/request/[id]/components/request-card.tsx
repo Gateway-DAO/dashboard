883149ba8ae3341ac7d88d745fd3a75c246bf64b
andrefelizardo <andreluis.felizardo@outlook.com>
2023-09-20 14:20:03 -0300

    WIP: mutation accept request

--- src/app/(light)/dashboard/user/request/[id]/components/request-card.tsx
+++ src/app/(light)/dashboard/user/request/[id]/components/request-card.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next-nprogress-bar';
 import Link from 'next/link';
 
 import GTWAvatar from '@/components/gtw-avatar/gtw-avatar';
+import Loading from '@/components/loadings/loading/loading';
 import { mutations } from '@/constants/queries';
 import routes from '@/constants/routes';
 import { useGtwSession } from '@/context/gtw-session-provider';
@@ -46,98 +47,101 @@ export default function RequestCard({
   });
 
   return (
-    <Box
-      sx={{
-        backgroundColor: (theme) => {
-          const color =
-            status === DataResourceStatus.Rejected
-              ? theme.palette.error.main
-              : theme.palette.primary.main;
-          const focusOpacity =
-            status === DataResourceStatus.Rejected
-              ? theme.palette.action.disabledOpacity
-              : theme.palette.action.focusOpacity;
-          return alpha(color, focusOpacity);
-        },
-        borderRadius: 1,
-      }}
-    >
-      <Box sx={{ p: 2 }}>
-        <Typography
-          component="p"
-          variant="caption"
-          color="text.secondary"
-          sx={{ mb: 2 }}
-        >
-          {request.request_card.title}
-        </Typography>
-        <Stack direction="row" alignItems="center" gap={2}>
-          <GTWAvatar name="chase" />{' '}
-          <Typography variant="h5">{requester}</Typography>
-        </Stack>
+    <>
+      {acceptDataRequest.isLoading && <Loading />}
+      <Box
+        sx={{
+          backgroundColor: (theme) => {
+            const color =
+              status === DataResourceStatus.Rejected
+                ? theme.palette.error.main
+                : theme.palette.primary.main;
+            const focusOpacity =
+              status === DataResourceStatus.Rejected
+                ? theme.palette.action.disabledOpacity
+                : theme.palette.action.focusOpacity;
+            return alpha(color, focusOpacity);
+          },
+          borderRadius: 1,
+        }}
+      >
+        <Box sx={{ p: 2 }}>
+          <Typography
+            component="p"
+            variant="caption"
+            color="text.secondary"
+            sx={{ mb: 2 }}
+          >
+            {request.request_card.title}
+          </Typography>
+          <Stack direction="row" alignItems="center" gap={2}>
+            <GTWAvatar name="chase" />{' '}
+            <Typography variant="h5">{requester}</Typography>
+          </Stack>
+        </Box>
+        <Divider />
+        <Box sx={{ p: 2 }}>
+          {status === DataResourceStatus.Pending && (
+            <>
+              <Typography variant="h5">
+                {request.request_card.content.pending.title}
+              </Typography>
+              <Typography>
+                {request.request_card.content.pending.description(requester)}
+              </Typography>
+              <Stack direction="row" gap={1} sx={{ mt: 3 }}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() =>
+                    acceptDataRequest.mutate({ requestId: requestId })
+                  }
+                >
+                  {common.actions.accept}
+                </Button>
+                <Button variant="outlined" color="primary">
+                  {common.actions.reject}
+                </Button>
+              </Stack>
+            </>
+          )}
+          {status === DataResourceStatus.Rejected && (
+            <>
+              <Typography variant="h5">
+                {request.request_card.content.rejected.title}
+              </Typography>
+              <Typography>
+                {request.request_card.content.rejected.description(requester)}
+              </Typography>
+              <Stack direction="row" gap={1} sx={{ mt: 3 }}>
+                <Button variant="outlined" color="inherit">
+                  {common.actions.learn_more}
+                </Button>
+              </Stack>
+            </>
+          )}
+          {status === DataResourceStatus.Accepted && (
+            <>
+              <Typography variant="h5">
+                {request.request_card.content.accepted.title}
+              </Typography>
+              <Typography>
+                {request.request_card.content.accepted.description(requester)}
+              </Typography>
+              <Stack direction="row" gap={1} sx={{ mt: 3 }}>
+                <Button
+                  component={Link}
+                  href={routes.dashboardUserProof(proofId ?? '')}
+                  variant="contained"
+                  color="primary"
+                >
+                  {common.actions.check_data_proof}
+                </Button>
+              </Stack>
+            </>
+          )}
+        </Box>
       </Box>
-      <Divider />
-      <Box sx={{ p: 2 }}>
-        {status === DataResourceStatus.Pending && (
-          <>
-            <Typography variant="h5">
-              {request.request_card.content.pending.title}
-            </Typography>
-            <Typography>
-              {request.request_card.content.pending.description(requester)}
-            </Typography>
-            <Stack direction="row" gap={1} sx={{ mt: 3 }}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() =>
-                  acceptDataRequest.mutate({ requestId: requestId })
-                }
-              >
-                {common.actions.accept}
-              </Button>
-              <Button variant="outlined" color="primary">
-                {common.actions.reject}
-              </Button>
-            </Stack>
-          </>
-        )}
-        {status === DataResourceStatus.Rejected && (
-          <>
-            <Typography variant="h5">
-              {request.request_card.content.rejected.title}
-            </Typography>
-            <Typography>
-              {request.request_card.content.rejected.description(requester)}
-            </Typography>
-            <Stack direction="row" gap={1} sx={{ mt: 3 }}>
-              <Button variant="outlined" color="inherit">
-                {common.actions.learn_more}
-              </Button>
-            </Stack>
-          </>
-        )}
-        {status === DataResourceStatus.Accepted && (
-          <>
-            <Typography variant="h5">
-              {request.request_card.content.accepted.title}
-            </Typography>
-            <Typography>
-              {request.request_card.content.accepted.description(requester)}
-            </Typography>
-            <Stack direction="row" gap={1} sx={{ mt: 3 }}>
-              <Button
-                component={Link}
-                href={routes.dashboardUserProof(proofId ?? '')}
-                variant="contained"
-                color="primary"
-              >
-                {common.actions.check_data_proof}
-              </Button>
-            </Stack>
-          </>
-        )}
-      </Box>
-    </Box>
+    </>
   );
 }
