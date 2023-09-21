2949cc0738b0cb005f19dfc27499192dcd1e8289
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-21 12:13:52 -0300

    feat: multiple middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,15 @@
-export { default } from 'next-auth/middleware';
+import { NextRequest } from 'next/server';
 
-export const config = { matcher: ['/dashboard/(.*)'] };
+import authenticatedMiddleware, {
+  authenticatedMatcher,
+} from './middlewares/authenticated';
+
+export default async function middleware(req: NextRequest) {
+  if (req.url.match(authenticatedMatcher)) {
+    return (authenticatedMiddleware as any)(req);
+  }
+}
+
+export const config = {
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
+};
