ae4ad7483281cbe9426f00f04b5cd33663b876eb
andrefelizardo <andreluis.felizardo@outlook.com>
2023-09-22 19:46:27 -0300

    fix: display profile picture on data request table

--- src/app/(light)/dashboard/user/requests/components/requests-table.tsx
+++ src/app/(light)/dashboard/user/requests/components/requests-table.tsx
@@ -48,6 +48,7 @@ const columns: GridColDef<PartialDeep<DataRequest>>[] = [
         <Stack direction="row" alignItems="center" spacing={1.5}>
           <GTWAvatar
             name={params.row.userVerifier!.gatewayId! || ''}
+            src={params.row.userVerifier?.profilePicture}
             size={32}
           />
           <Typography variant="body2">
