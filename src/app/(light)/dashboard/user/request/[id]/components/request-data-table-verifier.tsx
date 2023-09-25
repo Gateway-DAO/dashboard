ccc2f426878aed588514fb9d6229c3019ea9c1f7
andrefelizardo <andreluis.felizardo@outlook.com>
2023-09-25 13:36:48 -0300

    feat: display verifier view on data request

--- src/app/(light)/dashboard/user/request/[id]/components/request-data-table-verifier.tsx
+++ src/app/(light)/dashboard/user/request/[id]/components/request-data-table-verifier.tsx
@@ -1,35 +1,28 @@
 import { DataModel } from '@/services/protocol/types';
 import { PartialDeep } from 'type-fest';
 
-import { Stack } from '@mui/material';
+import { Stack } from '@mui/system';
 
-import RequestedData from './requested-data';
+import RequestDataVerifierView from './request-data-verifier';
 import { addDataModelDataToSchema } from './utils';
 
 type Props = {
   schema: any[];
-  validData: any[] | null;
   dataModels: PartialDeep<DataModel>[];
 };
 
-export default function RequestDataTable({
+export default function RequestDataTableVerifierView({
   schema,
-  validData,
   dataModels,
 }: Props) {
   const schemaJoin =
     schema && schema.length > 0
       ? addDataModelDataToSchema(schema, dataModels)
       : schema;
-
   return (
     <Stack direction="column" gap={2} mt={2}>
-      {schemaJoin.map((dataModel: any, index: number) => (
-        <RequestedData
-          key={dataModel.id}
-          dataModel={dataModel}
-          validDataProvided={validData?.[index]}
-        />
+      {schemaJoin.map((dataModel: any) => (
+        <RequestDataVerifierView key={dataModel.id} dataModel={dataModel} />
       ))}
     </Stack>
   );
---
+++ src/app/(light)/dashboard/user/request/[id]/components/utils.ts
@@ -0,0 +1,43 @@
+export const addDataModelDataToSchema = (schema: any, dataModels: any[]) => {
+  for (const schemaItem of schema) {
+    const matchingDataModel = dataModels?.find(
+      (dataModel) => dataModel.id === schemaItem.id
+    );
+    if (matchingDataModel) {
+      schemaItem.title = matchingDataModel.title;
+      schemaItem.schema = matchingDataModel.schema;
+    }
+  }
+
+  return schema;
+};
+
+type propertiesArrayItem = {
+  propertyName: string;
+  title: string;
+  type: string;
+  validations: any;
+};
+
+// Function to translate claimValidations into a object easy to run and read
+export const createPropertiesArray = (
+  dataModel: any
+): propertiesArrayItem[] => {
+  const propertiesArray = [];
+  for (const propertyName in dataModel.claimValidations?.properties) {
+    const propertyObj = dataModel.claimValidations?.properties[propertyName];
+    const validations = Object.keys(propertyObj)
+      .filter((key) => key !== 'type' && key !== 'title')
+      .map((key) => `${key} ${propertyObj[key]}`)
+      .join(', ');
+
+    propertiesArray.push({
+      propertyName,
+      title: dataModel.schema?.properties?.[propertyName]?.title,
+      type: propertyObj.type,
+      validations,
+    });
+  }
+
+  return propertiesArray;
+};
