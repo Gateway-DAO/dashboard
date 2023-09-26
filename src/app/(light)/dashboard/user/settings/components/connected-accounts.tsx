fc98e970f8ae581d80a55f85661525f76d50e7b7
João Conserva <joao@mygateway.xyz>
2023-09-26 20:48:11 -0300

    chore: created hook to handle disconnect aliases

--- src/app/(light)/dashboard/user/settings/components/connected-accounts.tsx
+++ src/app/(light)/dashboard/user/settings/components/connected-accounts.tsx
@@ -1,17 +1,15 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next-nprogress-bar';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
 import { LoadingButton } from '@/components/buttons/loading-button/loading-button';
 import ModalRight from '@/components/modal/modal-right/modal-right';
 import ModalTitle from '@/components/modal/modal-title/modal-title';
-import routes from '@/constants/routes';
+import { AliasType, useDisconnectAlias } from '@/hooks/use-disconnect-alias';
 import { settings } from '@/locale/en/settings';
 import { AuthType } from '@/services/protocol/types';
 import { NEGATIVE_CONTAINER_PX } from '@/theme/config/style-tokens';
-import { useToggle } from '@react-hookz/web/cjs/useToggle';
 
 import { Box, Divider, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
@@ -20,22 +18,14 @@ import EmailsSection from './account-sections/emails-section';
 import SocialsSection from './account-sections/socials-section';
 import WalletsSection from './account-sections/wallets-section';
 
-export type AliasType =
-  | 'email'
-  | 'wallet'
-  | 'Google'
-  | 'Discord'
-  | 'Github'
-  | 'Twitter';
-
 export default function ConnectedAccounts() {
   const { data: session } = useSession();
-  const router = useRouter();
-  const [openModalRight, setOpenModalRight] = useToggle(false);
-  const [dataToDisconnect, setDataToDisconnect] = useState<{
-    type: AliasType | undefined;
-    address: string | undefined;
-  } | null>(null);
+  const {
+    handleDisconnectAlias,
+    deactivateGatewayId,
+    closeModal,
+    openModalRight,
+  } = useDisconnectAlias();
 
   const wallets = useMemo(() => {
     return (
@@ -51,69 +41,6 @@ export default function ConnectedAccounts() {
       []
     );
   }, [session]);
-
-  const openModal = () => {
-    router.push(`#deactivate-gateway-id`, { scroll: false });
-    setOpenModalRight();
-  };
-
-  const closeModal = () => {
-    router.push(routes.dashboardUserSettings, { scroll: false });
-    setOpenModalRight();
-  };
-
-  const deactivateGatewayId = () => {
-    disconnectAlias({
-      type: dataToDisconnect?.type as AliasType,
-      address: dataToDisconnect?.address,
-    });
-    console.log('Gateway ID deactivated!');
-    setDataToDisconnect(null);
-  };
-
-  const handleDisconnectAlias = ({
-    type,
-    address,
-  }: {
-    type: AliasType;
-    address?: string;
-  }) => {
-    if (session?.user?.authentications?.length !== 1) {
-      openModal();
-      setDataToDisconnect({ type, address });
-    } else {
-      disconnectAlias({ type, address });
-    }
-  };
-
-  const disconnectAlias = ({
-    type,
-    address,
-  }: {
-    type: AliasType;
-    address?: string;
-  }) => {
-    if (type === 'email') disconnectEmail(address as string);
-    if (type === 'wallet') disconnectWallet(address as string);
-    if (type === 'Discord') disconnectDiscord();
-    if (type === 'Twitter') disconnectTwitter();
-  };
-
-  const disconnectEmail = (address: string) => {
-    console.log('Email disconnected!', address);
-  };
-
-  const disconnectWallet = (address: string) => {
-    console.log('Wallet disconnected!', address);
-  };
-
-  const disconnectDiscord = () => {
-    console.log('Discord disconnected!');
-  };
-
-  const disconnectTwitter = () => {
-    console.log('Twitter disconnected!');
-  };
 
   return (
     <Box
---
+++ src/hooks/use-disconnect-alias.tsx
@@ -0,0 +1,84 @@
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next-nprogress-bar';
+import { useState } from 'react';
+
+import routes from '@/constants/routes';
+import { useToggle } from '@react-hookz/web';
+
+export type AliasType =
+  | 'email'
+  | 'wallet'
+  | 'Google'
+  | 'Discord'
+  | 'Github'
+  | 'Twitter';
+
+type Alias = {
+  type: AliasType;
+  address?: string;
+};
+
+export function useDisconnectAlias() {
+  const { data: session } = useSession();
+  const [openModalRight, setOpenModalRight] = useToggle(false);
+  const [dataToDisconnect, setDataToDisconnect] = useState<Alias | null>(null);
+  const router = useRouter();
+
+  const openModal = () => {
+    router.push(`#deactivate-gateway-id`, { scroll: false });
+    setOpenModalRight();
+  };
+
+  const closeModal = () => {
+    router.push(routes.dashboardUserSettings, { scroll: false });
+    setOpenModalRight();
+  };
+
+  const deactivateGatewayId = () => {
+    disconnectAlias({
+      type: dataToDisconnect?.type as AliasType,
+      address: dataToDisconnect?.address,
+    });
+    console.log('Gateway ID deactivated!');
+    setDataToDisconnect(null);
+  };
+
+  const handleDisconnectAlias = ({ type, address }: Alias) => {
+    if (session?.user?.authentications?.length === 1) {
+      openModal();
+      setDataToDisconnect({ type, address });
+    } else {
+      disconnectAlias({ type, address });
+    }
+  };
+
+  const disconnectAlias = ({ type, address }: Alias) => {
+    if (type === 'email') disconnectEmail(address as string);
+    if (type === 'wallet') disconnectWallet(address as string);
+    if (type === 'Discord') disconnectDiscord();
+    if (type === 'Twitter') disconnectTwitter();
+  };
+
+  const disconnectEmail = (address: string) => {
+    console.log('Email disconnected!', address);
+  };
+
+  const disconnectWallet = (address: string) => {
+    console.log('Wallet disconnected!', address);
+  };
+
+  const disconnectDiscord = () => {
+    console.log('Discord disconnected!');
+  };
+
+  const disconnectTwitter = () => {
+    console.log('Twitter disconnected!');
+  };
+
+  return {
+    handleDisconnectAlias,
+    deactivateGatewayId,
+    closeModal,
+    openModalRight,
+  };
+}
