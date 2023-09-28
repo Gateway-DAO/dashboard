e629ba2766b23cb1917a23e74b8785d8d76a5fcf
Gustavo Cardoso <g.cardoso23@outlook.com>
2023-09-28 17:40:00 -0300

    fix: landing - lenis class added manually

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="lenis lenis-smooth">
       <body>
         <Main>
           <Providers>{children}</Providers>
