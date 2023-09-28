d8292b28dd41ef628edb8c315da4550be7e2db47
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-09-28 12:13:59 -0300

    feat: progress bar on landing

--- src/app/(landing)/providers.tsx
+++ src/app/(landing)/providers.tsx
@@ -1,9 +1,20 @@
 'use client';
 
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 import { PropsWithChildren } from 'react';
 
 import { ThemeProvider } from '@/theme';
 
 export default function Providers({ children }: PropsWithChildren) {
-  return <ThemeProvider dark>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider dark>
+      <ProgressBar
+        height="4px"
+        color="#771AC9"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+      {children}
+    </ThemeProvider>
+  );
 }
