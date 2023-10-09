72bdeb1aa7bd95cc192fd9eeb41d7ceaaed03a58
andrefelizardo <andreluis.felizardo@outlook.com>
2023-10-09 15:21:05 -0300

    fix: proofs dates

--- src/app/(light)/dashboard/org/[username]/proofs/received/components/org-proofs-received-table.tsx
+++ src/app/(light)/dashboard/org/[username]/proofs/received/components/org-proofs-received-table.tsx
@@ -67,7 +67,7 @@ const columns: GridColDef<PartialDeep<Proof>>[] = [
     },
   },
   {
-    field: 'shareDate',
+    field: 'createdAt',
     headerName: proofs.share_date,
     flex: 1,
     valueFormatter: (params) =>
