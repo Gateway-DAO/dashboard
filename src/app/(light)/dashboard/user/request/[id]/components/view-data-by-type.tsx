4a3459f8e7f4c25c5d8a6e28ce6490347e43ae91
andrefelizardo <andreluis.felizardo@outlook.com>
2023-10-11 18:49:16 -0300

    fix: boolean type

--- src/app/(light)/dashboard/user/request/[id]/components/view-data-by-type.tsx
+++ src/app/(light)/dashboard/user/request/[id]/components/view-data-by-type.tsx
@@ -7,16 +7,20 @@ export default function ViewDataByType({
   propertyType: string;
   currentProperties: any;
 }) {
+  if (
+    !currentProperties &&
+    JSON.stringify(currentProperties) === 'false' &&
+    JSON.stringify(currentProperties) === 'true'
+  ) {
+    return <Typography color="error">It doesn't met the criteria</Typography>;
+  }
+
   if (propertyType === 'boolean') {
     if (JSON.stringify(currentProperties) === 'false') {
       return <>FALSE</>;
     } else {
       return <>TRUE</>;
     }
-  }
-
-  if (!currentProperties) {
-    return <Typography color="error">It doesn't met the criteria</Typography>;
   }
 
   if (propertyType === 'array') {
