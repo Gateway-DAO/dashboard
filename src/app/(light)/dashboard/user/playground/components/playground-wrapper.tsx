34222ac59d8edb4b322f7937f7ac6461b6dab1e8
andrefelizardo <andreluis.felizardo@outlook.com>
2023-10-12 17:23:09 -0300

    feat: api-key and dynamic auth bearer

--- src/app/(light)/dashboard/user/playground/components/playground-wrapper.tsx
+++ src/app/(light)/dashboard/user/playground/components/playground-wrapper.tsx
@@ -1,10 +1,43 @@
 'use client';
 
+import { useGtwSession } from '@/context/gtw-session-provider';
 import { ApolloSandbox } from '@apollo/sandbox/react';
 
 import { Stack } from '@mui/material';
 
+const initialQuery = `mutation createPDA {
+  createPDA(
+      input: {
+          title: "Hello Gateway"
+          description: "This is the first PDA I have issued with Gateway!"
+          owner: {
+              type: GATEWAY_ID
+              value: "sanket"
+          }
+          dataModelId: "f4014d53-b30f-4490-9812-cea379a1b398"
+          image: "https://cdn.mygateway.xyz/logo.png"
+          expirationDate: null
+          claim: {gatewayUse: "To make data move with you"}
+      }
+  ) {
+      id
+      arweaveUrl
+      dataAsset {
+          owner {
+              id
+              gatewayId
+          }
+          issuer {
+              id
+              gatewayId
+          }
+      }
+  }
+}`;
+
 export default function PlaygroundWrapper() {
+  const { session } = useGtwSession();
+
   return (
     <Stack
       sx={{
@@ -14,7 +47,16 @@ export default function PlaygroundWrapper() {
         },
       }}
     >
-      <ApolloSandbox initialEndpoint="https://develop.protocol.mygateway.xyz/v1/graphql" />
+      <ApolloSandbox
+        initialState={{
+          sharedHeaders: {
+            'x-api-key': process.env.NEXT_PUBLIC_API_PLAYGROUND_KEY!,
+            Authorization: `Bearer ${session.token}`,
+          },
+          document: initialQuery,
+        }}
+        initialEndpoint="https://develop.protocol.mygateway.xyz/v1/graphql"
+      />
     </Stack>
   );
 }
