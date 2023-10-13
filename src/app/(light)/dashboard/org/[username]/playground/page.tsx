40d640a52646268dee8e6c60e713676195ab57f0
andrefelizardo <andreluis.felizardo@outlook.com>
2023-10-13 18:24:29 -0300

    feat: developer links as org

--- src/app/(light)/dashboard/org/[username]/playground/page.tsx
+++ src/app/(light)/dashboard/org/[username]/playground/page.tsx
@@ -1,8 +1,11 @@
 import TitleLayout from '@/components/title-layout/title-layout';
 import { playground } from '@/locale/en/playground';
 
+import { Alert, Typography } from '@mui/material';
+
 import DocumentationLinks from './components/documentation-links';
 import PlaygroundWrapper from './components/playground-wrapper';
+import UsageAlert from './components/usage-alert';
 
 export default async function OrgPlaygroundPage() {
   return (
@@ -13,6 +16,7 @@ export default async function OrgPlaygroundPage() {
         subtitle={playground.subtitle}
       />
       <DocumentationLinks />
+      <UsageAlert />
       <PlaygroundWrapper />
     </>
   );
