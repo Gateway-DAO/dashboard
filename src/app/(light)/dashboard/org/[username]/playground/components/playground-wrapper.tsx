1d64cc7cb859840a6f80fb09c86cf824869f0135
andrefelizardo <andreluis.felizardo@outlook.com>
2023-10-13 18:24:29 -0300

    feat: developer links as org

--- src/app/(light)/dashboard/org/[username]/playground/components/playground-wrapper.tsx
+++ src/app/(light)/dashboard/org/[username]/playground/components/playground-wrapper.tsx
@@ -5,6 +5,16 @@ import { ApolloSandbox } from '@apollo/sandbox/react';
 
 import { Stack } from '@mui/material';
 
+const dataModelIdByEnv = {
+  development: 'f4014d53-b30f-4490-9812-cea379a1b398',
+  staging: '478bbfb8-c6e2-4f2b-8b40-2ecb81757441',
+  testnet: '01ef1d89-c66d-49ff-8f43-167ca208ca04',
+};
+
+const currentDataModel = dataModelIdByEnv[
+  process.env.NEXT_PUBLIC_API_ENDPOINT as 'development' | 'staging' | 'testnet'
+] as string;
+
 const initialQuery = `mutation createPDA {
   createPDA(
       input: {
@@ -14,7 +24,7 @@ const initialQuery = `mutation createPDA {
               type: GATEWAY_ID
               value: "sanket"
           }
-          dataModelId: "f4014d53-b30f-4490-9812-cea379a1b398"
+          dataModelId: ${currentDataModel}
           image: "https://cdn.mygateway.xyz/logo.png"
           expirationDate: null
           claim: {gatewayUse: "To make data move with you"}
