42c9d7846284eb406ce2c6e0cf8762521e1aa702
João Conserva <boscoj@outlook.com>
2023-10-16 15:58:41 -0300

    fix: fixed top text

--- src/app/(landing)/components/header/index.tsx
+++ src/app/(landing)/components/header/index.tsx
@@ -60,150 +60,150 @@ export default function Header() {
   }, [burgerActive]);
 
   return (
-    <nav
-      className={joinClasses(
-        styles.element,
-        styles[`element--${scrollDirection}`],
-        styles[`element--${variant}`],
-        styles[`element--${isMobile === null && isTablet === null && 'hide'}`]
-      )}
-      ref={navRef}
-    >
+    <>
       <Stack
+        direction="row"
+        gap={0.5}
         sx={{
-          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
           background: '#E6D5FA',
           height: 40,
-          alignItems: 'center',
           flexDirection: 'row',
+          '& a': { textDecoration: 'none' },
         }}
       >
-        <Wrapper className={styles.wrapper}>
-          <Stack
-            direction="row"
-            gap={0.5}
-            sx={{ alignItems: 'center', '& a': { textDecoration: 'none' } }}
-          >
-            <Typography color="common.black">
-              Product Marketing Engagements can now be found at
-            </Typography>
-            <Link href="https://tryodyssey.xyz">
-              <Typography color="primary">tryodyssey.xyz</Typography>
-            </Link>
-          </Stack>
-        </Wrapper>
+        <Typography color="common.black">
+          Product Marketing Engagements can now be found at
+        </Typography>
+        <Link href="https://tryodyssey.xyz">
+          <Typography color="primary">tryodyssey.xyz</Typography>
+        </Link>
       </Stack>
 
-      <Wrapper className={styles.wrapper}>
-        {isMobile || isTablet ? (
-          <>
-            <Link href="/" className={styles.logo_link}>
-              <GatewayLogo variant={variant} />
+      <nav
+        className={joinClasses(
+          styles.element,
+          styles[`element--${scrollDirection}`],
+          styles[`element--${variant}`],
+          styles[`element--${isMobile === null && isTablet === null && 'hide'}`]
+        )}
+        ref={navRef}
+      >
+        <Wrapper className={styles.wrapper}>
+          {isMobile || isTablet ? (
+            <>
+              <Link href="/" className={styles.logo_link}>
+                <GatewayLogo variant={variant} />
+              </Link>
+
+              <div className={styles.mobile_buttons}>
+                <Link href="/login">
+                  <Button
+                    variant="contained"
+                    className={styles.mobile_button_head_dashboard}
+                  >
+                    Open dashboard
+                  </Button>
+                </Link>
+                <BurgerButton
+                  className={styles.mobile_burger}
+                  active={burgerActive}
+                  onClick={() => setBurgerActive(!burgerActive)}
+                  variant={variant}
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <Link href="/" className={styles.logo_link}>
+                <GatewayLogo withName variant={variant} />
+              </Link>
+
+              <div className={styles.links}>
+                <Link className={styles.link} href="/learn">
+                  <Button variant="text">Learn</Button>
+                </Link>
+                <Link className={styles.link} href="/build">
+                  <Button variant="text">Build</Button>
+                </Link>
+              </div>
+
+              <div className={styles.buttons_container}>
+                <Link href="/explorer">
+                  <Button className={styles.button_outlined} variant="outlined">
+                    Explorer
+                  </Button>
+                </Link>
+
+                <Link href="/login">
+                  <Button
+                    variant="contained"
+                    className={styles.button_contained}
+                  >
+                    Open dashboard
+                  </Button>
+                </Link>
+              </div>
+            </>
+          )}
+        </Wrapper>
+
+        <div
+          className={joinClasses(
+            styles.mobile_menu_mask,
+            burgerActive ? styles['mobile_menu_mask--open'] : ''
+          )}
+          ref={mobileMenuRef}
+        >
+          <Wrapper className={styles.mobile_wrapper}>
+            <Link
+              className={styles.mobile_link}
+              href="/learn"
+              onClick={() => setBurgerActive(false)}
+            >
+              <Button variant="text">
+                <span>Learn</span>
+                <ArrowRight2 className={styles.mobile_link_arrow} />
+              </Button>
+            </Link>
+            <Link
+              className={styles.mobile_link}
+              href="/build"
+              onClick={() => setBurgerActive(false)}
+            >
+              <Button variant="text">
+                <span>Build</span>
+                <ArrowRight2 className={styles.mobile_link_arrow} />
+              </Button>
             </Link>
 
-            <div className={styles.mobile_buttons}>
-              <Link href="/login">
+            <div className={styles.mobile_menu_buttons}>
+              <Link
+                className={styles.mobile_menu_button_link}
+                href="/login"
+                onClick={() => setBurgerActive(false)}
+              >
                 <Button
                   variant="contained"
-                  className={styles.mobile_button_head_dashboard}
+                  className={styles.mobile_menu_button_link_contained}
                 >
                   Open dashboard
                 </Button>
               </Link>
-              <BurgerButton
-                className={styles.mobile_burger}
-                active={burgerActive}
-                onClick={() => setBurgerActive(!burgerActive)}
-                variant={variant}
-              />
-            </div>
-          </>
-        ) : (
-          <>
-            <Link href="/" className={styles.logo_link}>
-              <GatewayLogo withName variant={variant} />
-            </Link>
-
-            <div className={styles.links}>
-              <Link className={styles.link} href="/learn">
-                <Button variant="text">Learn</Button>
-              </Link>
-              <Link className={styles.link} href="/build">
-                <Button variant="text">Build</Button>
-              </Link>
-            </div>
-
-            <div className={styles.buttons_container}>
-              <Link href="/explorer">
+              <Link
+                className={styles.mobile_menu_button_link}
+                href="/explorer"
+                onClick={() => setBurgerActive(false)}
+              >
                 <Button className={styles.button_outlined} variant="outlined">
                   Explorer
                 </Button>
               </Link>
-
-              <Link href="/login">
-                <Button variant="contained" className={styles.button_contained}>
-                  Open dashboard
-                </Button>
-              </Link>
             </div>
-          </>
-        )}
-      </Wrapper>
-
-      <div
-        className={joinClasses(
-          styles.mobile_menu_mask,
-          burgerActive ? styles['mobile_menu_mask--open'] : ''
-        )}
-        ref={mobileMenuRef}
-      >
-        <Wrapper className={styles.mobile_wrapper}>
-          <Link
-            className={styles.mobile_link}
-            href="/learn"
-            onClick={() => setBurgerActive(false)}
-          >
-            <Button variant="text">
-              <span>Learn</span>
-              <ArrowRight2 className={styles.mobile_link_arrow} />
-            </Button>
-          </Link>
-          <Link
-            className={styles.mobile_link}
-            href="/build"
-            onClick={() => setBurgerActive(false)}
-          >
-            <Button variant="text">
-              <span>Build</span>
-              <ArrowRight2 className={styles.mobile_link_arrow} />
-            </Button>
-          </Link>
-
-          <div className={styles.mobile_menu_buttons}>
-            <Link
-              className={styles.mobile_menu_button_link}
-              href="/login"
-              onClick={() => setBurgerActive(false)}
-            >
-              <Button
-                variant="contained"
-                className={styles.mobile_menu_button_link_contained}
-              >
-                Open dashboard
-              </Button>
-            </Link>
-            <Link
-              className={styles.mobile_menu_button_link}
-              href="/explorer"
-              onClick={() => setBurgerActive(false)}
-            >
-              <Button className={styles.button_outlined} variant="outlined">
-                Explorer
-              </Button>
-            </Link>
-          </div>
-        </Wrapper>
-      </div>
-    </nav>
+          </Wrapper>
+        </div>
+      </nav>
+    </>
   );
 }
