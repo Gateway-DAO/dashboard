2adbd9a67798d4a2dd1328ccc1c057e2a53e526d
Nuno Carvalho <nunoormaker21@gmail.com>
2023-10-17 20:14:47 +0100

    feat: add sandbox chip on logo

--- src/app/(light)/dashboard/components/logo/logo.tsx
+++ src/app/(light)/dashboard/components/logo/logo.tsx
@@ -1,17 +1,30 @@
 import GatewaySquaredIcon from '@/components/icons/gateway-squared';
 import { common } from '@/locale/en/common';
 
-import { Typography } from '@mui/material';
+import { Chip, Stack, Tooltip, Typography } from '@mui/material';
 
 import LogoContainer from './logo-container';
 
 export default function Logo() {
+  const { NEXT_PUBLIC_API_ENV } = process.env;
+
   return (
-    <LogoContainer>
-      <GatewaySquaredIcon sx={{ fontSize: 40 }} />
-      <Typography component="h1" ml={1} color="black" fontWeight="bold">
-        {common.general.gateway}
-      </Typography>
-    </LogoContainer>
+    <Stack direction="row" alignItems="center">
+      <LogoContainer>
+        <GatewaySquaredIcon sx={{ fontSize: 40 }} />
+        <Typography component="h1" ml={1} color="black" fontWeight="bold">
+          {common.general.gateway}
+        </Typography>
+      </LogoContainer>
+      {NEXT_PUBLIC_API_ENV === 'testnet' && (
+        <Tooltip
+          title="You are on the Gateway Sandbox. The data is temporary and will expire in 60 days."
+          placement="right"
+          arrow
+        >
+          <Chip color="warning" size="small" variant="filled" label="Sandbox" />
+        </Tooltip>
+      )}
+    </Stack>
   );
 }
