b0edde6c790553a9d4da74b55a35386f2f88cc57
Nuno Carvalho <nunoormaker21@gmail.com>
2023-10-18 15:21:31 +0100

    feat: add sandbox card to org routes

--- src/app/(light)/dashboard/org/[username]/assets/issued/layout.tsx
+++ src/app/(light)/dashboard/org/[username]/assets/issued/layout.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react';
 
+import SandboxAlert from '@/app/(light)/dashboard/components/alerts/sandbox-alert';
 import TitleLayout from '@/components/title-layout/title-layout';
 import { orgPdas } from '@/locale/en/pda';
 
@@ -8,6 +9,7 @@ import { Box } from '@mui/material';
 export default function OrgDataAssetsIssued({ children }: PropsWithChildren) {
   return (
     <Box sx={{ py: 2 }}>
+      {process.env.NEXT_PUBLIC_API_ENV === 'testnet' && <SandboxAlert />}
       <TitleLayout
         title={orgPdas.data_assets_title}
         subtitle={orgPdas.data_assets_subtitle}
--- src/app/(light)/dashboard/org/[username]/data-models/layout.tsx
+++ src/app/(light)/dashboard/org/[username]/data-models/layout.tsx
@@ -5,9 +5,12 @@ import { datamodels } from '@/locale/en/datamodel';
 
 import { Box } from '@mui/material';
 
+import SandboxAlert from '../../../components/alerts/sandbox-alert';
+
 export default function OrgDataModelsLayout({ children }: PropsWithChildren) {
   return (
     <Box sx={{ py: 2 }}>
+      {process.env.NEXT_PUBLIC_API_ENV === 'testnet' && <SandboxAlert />}
       <TitleLayout
         titleId={datamodels.id}
         title={datamodels.title}
