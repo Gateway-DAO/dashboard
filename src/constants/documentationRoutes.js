22d5781ceab63cdf4ebbac9fbcf5bdac96c26e79
andrefelizardo <andreluis.felizardo@outlook.com>
2023-10-19 19:23:35 -0300

    feat: GTW-831

--- src/constants/documentationRoutes.js
+++ src/constants/documentationRoutes.js
@@ -2,6 +2,7 @@ const documentationRoutes = {
   home: 'https://docs.mygateway.xyz/docs/get-started-here',
   createPDA: 'https://apidocs.mygateway.xyz/#mutation-createPDA',
   me: 'https://apidocs.mygateway.xyz/#query-me',
+  dataModel: 'https://docs.mygateway.xyz/docs/data-model',
 };
 
 module.exports = documentationRoutes;
