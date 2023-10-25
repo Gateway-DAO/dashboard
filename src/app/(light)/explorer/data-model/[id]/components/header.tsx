ff8ed1e7f3d77104e1fbb55c5be987d8ada63fa7
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-10-25 15:41:14 -0300

    feat: started data model internal pages

---
+++ src/app/(light)/explorer/data-model/[id]/components/header.tsx
@@ -0,0 +1,70 @@
+import GTWTab from '@/components/tabs/gtw-tab';
+import GTWTabs from '@/components/tabs/gtw-tabs-links';
+import routes from '@/constants/routes';
+import { common } from '@/locale/en/common';
+import { DataModel } from '@/services/protocol/types';
+import { PartialDeep } from 'type-fest';
+
+import { Chip, Container, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
+
+import ExplorerBreadcrumb from '../../../components/breadcrumb/breadcrumb';
+import ExplorerHeader from '../../../components/header/header';
+
+type Props = {
+  dataModel: PartialDeep<DataModel>;
+};
+
+export default function DataModelDetailHeader({ dataModel }: Props) {
+  return (
+    <>
+      <ExplorerHeader sx={{ pb: 5 }}>
+        <Container>
+          <ExplorerBreadcrumb
+            paths={[
+              {
+                route: routes.explorerDataModels,
+                label: 'Data models',
+              },
+              {
+                label: dataModel.title!,
+              },
+            ]}
+          />
+          <Typography component="h1" variant="h2" fontWeight="300" mb={2}>
+            {dataModel.title}
+          </Typography>
+          {dataModel.tags && (
+            <Stack direction="row" gap={1}>
+              {dataModel.tags.map((tag) => (
+                <Chip key={tag} label={tag} />
+              ))}
+            </Stack>
+          )}
+        </Container>
+      </ExplorerHeader>
+      <Container
+        sx={{
+          borderBottom: 1,
+          borderColor: 'divider',
+          mb: 5,
+        }}
+      >
+        <GTWTabs>
+          <GTWTab
+            label={common.general.overview}
+            href={routes.explorerDataModel(dataModel.id)}
+          />
+          <GTWTab
+            label={common.general.issued}
+            href={routes.explorerDataModelIssuers(dataModel.id)}
+          />
+          <GTWTab
+            label={common.general.received}
+            href={routes.explorerDataModelRequestTemplates(dataModel.id)}
+          />
+        </GTWTabs>
+      </Container>
+    </>
+  );
+}
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -51,6 +51,10 @@ const routes = {
   explorer: '/explorer',
   explorerDataModels: '/explorer/data-models',
   explorerDataModel: (dataModelId) => `/explorer/data-model/${dataModelId}`,
+  explorerDataModelIssuers: (dataModelId) =>
+    `/explorer/data-model/${dataModelId}/issuers`,
+  explorerDataModelRequestTemplates: (dataModelId) =>
+    `/explorer/data-model/${dataModelId}/request-templates`,
 };
 
 module.exports = routes;
