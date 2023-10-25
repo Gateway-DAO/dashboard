e0d32aad24bef33431218423a079d19cd1284345
João Conserva <boscoj@outlook.com>
2023-10-25 10:48:01 -0300

    chore: created an example to use transaction modal

---
+++ src/components/transaction/components/transaction-modal.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { useRouter } from 'next-nprogress-bar';
+
+import ModalHeader from '@/components/modal/modal-header/modal-header';
+import ModalRight from '@/components/modal/modal-right/modal-right';
+import routes from '@/constants/routes';
+import { useToggle } from '@react-hookz/web';
+
+import { Button } from '@mui/material';
+import { Stack } from '@mui/system';
+
+import { Transaction } from '../transaction';
+
+export function TransactionModal() {
+  const router = useRouter();
+  const [openTransaction, setOpenTransaction] = useToggle(false);
+
+  const toggleModal = () => {
+    if (openTransaction) {
+      router.push(routes.dashboardUserHome, { scroll: false });
+    } else {
+      router.push('#transaction', { scroll: false });
+    }
+    setOpenTransaction();
+  };
+
+  return (
+    <>
+      <Button onClick={toggleModal}>Toggle Modal</Button>
+      <ModalRight open={openTransaction} onClose={toggleModal}>
+        <ModalHeader onClose={toggleModal} />
+        <Stack sx={{ pt: 3 }}>
+          <Transaction />
+        </Stack>
+      </ModalRight>
+    </>
+  );
+}
