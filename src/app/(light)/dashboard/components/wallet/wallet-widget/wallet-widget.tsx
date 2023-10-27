2bfeeec109f44a49e7cb26fcb89326dd5c81c6c1
João Conserva <boscoj@outlook.com>
2023-10-27 12:20:18 -0300

    chore: added validation to show or hidden the wallet widget

--- src/app/(light)/dashboard/components/wallet/wallet-widget/wallet-widget.tsx
+++ src/app/(light)/dashboard/components/wallet/wallet-widget/wallet-widget.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import routes from '@/constants/routes';
 import useOrganization from '@/hooks/use-organization';
 import { common } from '@/locale/en/common';
 import { wallet } from '@/locale/en/wallet';
+import { numberToMoneyString } from '@/utils/money';
 import { useToggle } from '@react-hookz/web';
 
 import {
@@ -16,8 +19,6 @@ import {
 import { Button, Box, Stack, Typography, alpha } from '@mui/material';
 
 import WalletWidgetSkeleton from './wallet-widget-skeleton';
-import Link from 'next/link';
-import { numberToMoneyString } from '@/utils/money';
 
 type Props = {
   id: string;
@@ -27,6 +28,7 @@ export default function WalletWidget({ id }: Props) {
   const { data: session, status } = useSession();
   const [visible, setVisible] = useToggle(true);
   const { organization } = useOrganization();
+  const pathname = usePathname();
 
   if (status === 'loading' || !session) {
     return <WalletWidgetSkeleton />;
@@ -36,73 +38,77 @@ export default function WalletWidget({ id }: Props) {
     value: 0.3,
   };
 
+  const walletPage = !!organization
+    ? routes.dashboardOrgWallet(organization.gatewayId)
+    : routes.dashboardUserWallet;
+
   return (
-    <Box
-      id={id}
-      data-testid="wallet-widget"
-      sx={(theme) => ({
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.focusOpacity
-        ),
-        borderRadius: theme.shape.borderRadius / 16, //Strange issue with MUI
-        justifyContent: 'space-between',
-        p: 2,
-        mb: 2,
-        alignItems: 'flex-start',
-        flexDirection: 'column',
-      })}
-    >
-      <Stack
-        direction="row"
-        alignItems="center"
-        sx={{ justifyContent: 'space-between', width: '100%', mb: 2 }}
-      >
-        <Stack alignItems="flex-start">
-          <Typography variant="caption" color="primary" lineHeight={1}>
-            {wallet.sidebar_box.wallet_balance}
-          </Typography>
-          {visible ? (
-            <Typography
-              fontWeight={600}
-              color="primary"
-              data-testid="wallet-widget__value"
-            >
-              {numberToMoneyString(walletData.value)}
-            </Typography>
-          ) : (
-            <Stack sx={{ overflow: 'hidden', height: 24 }}>
-              <MoreHorizOutlined
-                sx={{ fontSize: 32, position: 'relative', top: -3 }}
-              />
-            </Stack>
-          )}
-        </Stack>
-        <Button
-          onClick={setVisible}
-          size="small"
-          sx={{ p: 0.5, m: 0, minWidth: 0 }}
+    <>
+      {pathname !== walletPage && (
+        <Box
+          id={id}
+          data-testid="wallet-widget"
+          sx={(theme) => ({
+            backgroundColor: alpha(
+              theme.palette.primary.main,
+              theme.palette.action.focusOpacity
+            ),
+            borderRadius: theme.shape.borderRadius / 16, //Strange issue with MUI
+            justifyContent: 'space-between',
+            p: 2,
+            mb: 2,
+            alignItems: 'flex-start',
+            flexDirection: 'column',
+          })}
         >
-          {visible ? (
-            <VisibilityOutlined color="primary" />
-          ) : (
-            <VisibilityOffOutlined color="primary" />
-          )}
-        </Button>
-      </Stack>
-      <Link
-        href={
-          !!organization
-            ? routes.dashboardOrgWallet(organization.gatewayId)
-            : routes.dashboardUserWallet
-        }
-        passHref
-        data-testid="wallet-widget__view-more"
-      >
-        <Button variant="outlined" fullWidth size="small">
-          {common.actions.view_more}
-        </Button>
-      </Link>
-    </Box>
+          <Stack
+            direction="row"
+            alignItems="center"
+            sx={{ justifyContent: 'space-between', width: '100%', mb: 2 }}
+          >
+            <Stack alignItems="flex-start">
+              <Typography variant="caption" color="primary" lineHeight={1}>
+                {wallet.sidebar_box.wallet_balance}
+              </Typography>
+              {visible ? (
+                <Typography
+                  fontWeight={600}
+                  color="primary"
+                  data-testid="wallet-widget__value"
+                >
+                  {numberToMoneyString(walletData.value)}
+                </Typography>
+              ) : (
+                <Stack sx={{ overflow: 'hidden', height: 24 }}>
+                  <MoreHorizOutlined
+                    sx={{ fontSize: 32, position: 'relative', top: -3 }}
+                  />
+                </Stack>
+              )}
+            </Stack>
+            <Button
+              onClick={setVisible}
+              size="small"
+              sx={{ p: 0.5, m: 0, minWidth: 0 }}
+            >
+              {visible ? (
+                <VisibilityOutlined color="primary" />
+              ) : (
+                <VisibilityOffOutlined color="primary" />
+              )}
+            </Button>
+          </Stack>
+          <Link
+            href={walletPage}
+            passHref
+            data-testid="wallet-widget__view-more"
+          >
+            <Button variant="outlined" fullWidth size="small">
+              {common.actions.view_more}
+            </Button>
+          </Link>
+        </Box>
+      )}
+    </>
   );
 }
