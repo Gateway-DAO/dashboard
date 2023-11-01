0716177fc547c523d3ec7b883b872f1b80e8b60b
r11manish <github.jmtss@gmail.com>
2023-11-01 23:20:08 +0530

    feat : add create org dialog

--- src/app/(light)/dashboard/components/auth-component/auth-dropdown-profiles-list.tsx
+++ src/app/(light)/dashboard/components/auth-component/auth-dropdown-profiles-list.tsx
@@ -1,18 +1,26 @@
 'use client';
 import { useSession } from 'next-auth/react';
+import { useState } from 'react';
 
 import GTWAvatar from '@/components/gtw-avatar/gtw-avatar';
 import MenuItemLink from '@/components/menu-item-link/menu-item-link';
 import routes from '@/constants/routes';
 import useOrganization from '@/hooks/use-organization';
+import { auth } from '@/locale/en/auth';
 
+import AddIcon from '@mui/icons-material/Add';
 import {
   Chip,
   Divider,
+  IconButton,
   ListItemIcon,
   ListItemText,
+  MenuItem,
+  Typography,
   alpha,
 } from '@mui/material';
+
+import CreateOrgDialog from '../create-layout-dialog';
 
 type Props = {
   onClose: () => void;
@@ -22,6 +30,7 @@ export default function AuthDropdownProfilesList({ onClose }: Props) {
   const { data: session } = useSession();
 
   const { isOrg, organization } = useOrganization();
+  const [isCreateOrgDialog, setCreateOrgDialog] = useState(false);
 
   if (!session) return null;
 
@@ -38,6 +47,7 @@ export default function AuthDropdownProfilesList({ onClose }: Props) {
 
   return (
     <>
+      <CreateOrgDialog open={isCreateOrgDialog} setOpen={setCreateOrgDialog} />
       {accessess?.map(({ organization }) => (
         <MenuItemLink
           href={routes.dashboard.org.home(organization.gatewayId)}
@@ -81,6 +91,24 @@ export default function AuthDropdownProfilesList({ onClose }: Props) {
           </ListItemText>
         </MenuItemLink>
       )}
+      <MenuItem
+        onClick={() => {
+          setCreateOrgDialog(true);
+        }}
+      >
+        <ListItemIcon>
+          <IconButton
+            sx={{
+              backgroundColor: 'primary.light',
+            }}
+          >
+            <AddIcon htmlColor="#771AC9" />
+          </IconButton>
+        </ListItemIcon>
+        <ListItemText secondary={auth.create_org.desc}>
+          <Typography variant="subtitle1">{auth.create_org.title}</Typography>
+        </ListItemText>
+      </MenuItem>
       <Divider />
     </>
   );
