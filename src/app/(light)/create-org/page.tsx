1f61b903c71506c7b336d6ddb29f8821c92a50a6
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-11-06 17:18:57 -0300

    fix: separated client layout

--- src/app/(light)/create-org/page.tsx
+++ src/app/(light)/create-org/page.tsx
@@ -1,8 +1,6 @@
-'use client';
 import { Metadata } from 'next';
-import { useRouter } from 'next-nprogress-bar';
 
-import AuthenticationLayout from '../login/components/authentication-layout';
+import CreateOrgLayout from './components/create-org-layout';
 import CreateOrgStructure from './components/structure';
 
 export const metadata: Metadata = {
@@ -10,14 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function CreateOrg() {
-  const router = useRouter();
   return (
-    <AuthenticationLayout
-      closeButonProps={{
-        onClick: () => router.back(),
-      }}
-    >
+    <CreateOrgLayout>
       <CreateOrgStructure />
-    </AuthenticationLayout>
+    </CreateOrgLayout>
   );
 }
