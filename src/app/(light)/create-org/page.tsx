54db1900374953f5aa09727914879839ef7135eb
andrefelizardo <andreluis.felizardo@outlook.com>
2023-11-06 11:28:47 -0300

    feat: add metadata title to create org page

--- src/app/(light)/create-org/page.tsx
+++ src/app/(light)/create-org/page.tsx
@@ -1,8 +1,13 @@
 'use client';
+import { Metadata } from 'next';
 import { useRouter } from 'next-nprogress-bar';
 
 import AuthenticationLayout from '../login/components/authentication-layout';
 import CreateOrgStructure from './components/structure';
+
+export const metadata: Metadata = {
+  title: 'Create Organization - Gateway Network',
+};
 
 export default function CreateOrg() {
   const router = useRouter();
