7f4635626076f6353c4f9862c7aba4fdb4a345d8
andrefelizardo <andreluis.felizardo@outlook.com>
2023-11-08 14:31:22 -0300

    feat: details with mocked data

--- src/app/(light)/explorer/transactions/[id]/components/details.tsx
+++ src/app/(light)/explorer/transactions/[id]/components/details.tsx
@@ -1,13 +1,17 @@
 'use client';
-
-import Link from 'next/link';
 import React from 'react';
 
-import CardCell, { CardCellContainer } from '@/components/card-cell/card-cell';
-import { TableCellContainer } from '@/components/containers/table-cell-container/table-cell-container';
-import CopyButton from '@/components/copy-button/copy-button';
+import ExternalLink from '@/components/external-link/external-link';
+import GTWAvatar from '@/components/gtw-avatar/gtw-avatar';
+import { TextStatusChip } from '@/components/text-status-chip/text-status-chip';
+import { DATE_FORMAT } from '@/constants/date';
 import { explorerQueries } from '@/constants/queries';
+import routes from '@/constants/routes';
+import { transaction_detail } from '@/locale/en/transaction';
+import { PdaStatus } from '@/services/protocol/types';
+import { numberToMoneyString } from '@/utils/money';
 import { useQuery } from '@tanstack/react-query';
+import dayjs from 'dayjs';
 
 import {
   Box,
@@ -20,65 +24,102 @@ import {
   Typography,
 } from '@mui/material';
 
+import CardRow from './card-row';
+
 type Props = {
   id: string;
+};
+
+type User = {
+  id: string;
+  gatewayId?: string;
+  name?: string;
+  image?: string;
 };
 
 type Transaction = {
   type: string;
   pda_id: string;
+  arweave_url: string;
+  issuer: User;
+  signed_by: User;
+  data_model_id: string;
+  created_at: string;
+  cost: number;
+  status: PdaStatus;
+};
+
+const wait = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
 const fetchTransaction = async (): Promise<Transaction> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const mock: Transaction = {
-        type: 'PDA issuance',
-        pda_id: '6cfd080c-58c0-4c28-b34a-dc0bd11b2ec6',
-      };
-      resolve(mock);
-    }, 1500);
-  });
+  await wait(1500);
+  const mock: Transaction = {
+    type: 'PDA issuance',
+    pda_id: '6cfd080c-58c0-4c28-b34a-dc0bd11b2ec6',
+    arweave_url: 'http://andrefelizardo.com.br',
+    issuer: {
+      id: 'c68d70c8-53fe-4f4c-a1a4-80064c3be0e1',
+      gatewayId: 'spotify',
+      name: 'Spotify',
+    },
+    signed_by: {
+      id: 'd7a05292-2c01-4e45-ae96-56fa7f25ad6b',
+      gatewayId: 'oxford',
+    },
+    data_model_id: '2cc6e362-c146-40c8-b1b8-6eace653121d',
+    created_at: '2023-10-10T18:51:29.941Z',
+    cost: 0.15,
+    status: PdaStatus.Valid,
+  };
+  return mock;
 };
 
-const CardCellRow = ({
-  title,
-  children,
-}: {
-  title: string;
-  children: React.ReactNode;
-}) => {
+type UserColum = {
+  isLoading: boolean;
+  user: User | undefined;
+};
+
+const UserColumn = ({ user, isLoading = true }: UserColum) => {
   return (
-    <TableCellContainer>
-      <CardCellContainer>
-        <Stack direction="row" alignItems="center" pb={1}>
-          <Typography variant="subtitle1" flex={1}>
-            {title}
-          </Typography>
-          <Box
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            flex={2}
-            gap={1}
-          >
-            {children}
-          </Box>
-        </Stack>
-      </CardCellContainer>
-    </TableCellContainer>
+    <Stack direction="row" gap={1.5} alignItems="center">
+      {isLoading ? (
+        <Skeleton variant="circular" width={40} height={40} />
+      ) : (
+        <GTWAvatar name={user?.id} src={user?.image} />
+      )}
+      <Box>
+        <Typography variant="subtitle1" lineHeight={1}>
+          {isLoading ? (
+            <Skeleton width={200} />
+          ) : (
+            user?.name ?? user?.gatewayId ?? user?.id
+          )}
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          {isLoading ? (
+            <Skeleton width={100} />
+          ) : user?.name ? (
+            `@${user?.gatewayId}`
+          ) : (
+            ''
+          )}
+        </Typography>
+      </Box>
+    </Stack>
   );
 };
 
 export default function TransactionDetails({ id }: Props) {
   const { data, isLoading } = useQuery({
     queryKey: [explorerQueries.transaction, id],
-    queryFn: () => fetchTransaction() as unknown as Transaction,
+    queryFn: async () => (await fetchTransaction()) as Transaction,
   });
 
   return (
     <Container sx={{ pb: 4 }}>
-      <Box sx={{ maxWidth: 896 }}>
+      <Box sx={{ maxWidth: 777 }}>
         <Stack
           component={Card}
           variant="outlined"
@@ -94,16 +135,22 @@ export default function TransactionDetails({ id }: Props) {
             />
           }
         >
-          <CardCellRow title="Transaction ID">
+          <CardRow title={transaction_detail.transaction_id}>
             <Typography variant="body1">
               {isLoading ? <Skeleton variant="text" width={400} /> : id}
             </Typography>
-            <CopyButton size="small" variant="text" text={id} />
-          </CardCellRow>
-          <CardCellRow title="Action">
+            {data && (
+              <ExternalLink
+                iconSxProps={{ fontSize: 20, color: 'text.primary' }}
+                href={data?.arweave_url as string}
+                text=""
+              />
+            )}
+          </CardRow>
+          <CardRow title={transaction_detail.action}>
             {isLoading ? <Skeleton width={100} /> : <Chip label={data?.type} />}
-          </CardCellRow>
-          <CardCellRow title="Private Data Asset ID">
+          </CardRow>
+          <CardRow title={transaction_detail.pda_id}>
             <Typography variant="body1">
               {isLoading ? (
                 <Skeleton variant="text" width={300} />
@@ -111,7 +158,52 @@ export default function TransactionDetails({ id }: Props) {
                 data?.pda_id
               )}
             </Typography>
-          </CardCellRow>
+          </CardRow>
+          <CardRow title={transaction_detail.issuer}>
+            <UserColumn user={data?.issuer} isLoading={isLoading} />
+          </CardRow>
+          <CardRow title={transaction_detail.signed_by}>
+            <UserColumn user={data?.signed_by} isLoading={isLoading} />
+          </CardRow>
+          <CardRow title={transaction_detail.data_model_id}>
+            <Typography variant="body1">
+              {isLoading ? (
+                <Skeleton variant="text" width={400} />
+              ) : (
+                data?.data_model_id
+              )}
+            </Typography>
+            {data && (
+              <ExternalLink
+                iconSxProps={{ fontSize: 20, color: 'text.primary' }}
+                href={routes.explorer.dataModel(data.data_model_id)}
+                text=""
+              />
+            )}
+          </CardRow>
+          <CardRow title={transaction_detail.created_at}>
+            <Typography variant="body1">
+              {isLoading ? (
+                <Skeleton variant="text" width={200} />
+              ) : (
+                dayjs(data?.created_at).format(DATE_FORMAT)
+              )}
+            </Typography>
+          </CardRow>
+          <CardRow title={transaction_detail.cost}>
+            {isLoading ? (
+              <Skeleton variant="text" width={100} />
+            ) : (
+              numberToMoneyString(data?.cost ?? 0)
+            )}
+          </CardRow>
+          <CardRow title={transaction_detail.status}>
+            {isLoading ? (
+              <Skeleton width={30} />
+            ) : (
+              <TextStatusChip status={data?.status as PdaStatus} />
+            )}
+          </CardRow>
         </Stack>
       </Box>
     </Container>
--- src/locale/en/transaction.ts
+++ src/locale/en/transaction.ts
@@ -8,3 +8,15 @@ export const transaction = {
   type: 'Type',
   earning: 'Earning',
 };
+
+export const transaction_detail = {
+  transaction_id: 'Transaction ID',
+  action: 'Action',
+  pda_id: 'Private Data Asset ID',
+  issuer: 'Issuer',
+  signed_by: 'Signed by',
+  data_model_id: 'Data model ID',
+  created_at: 'Created at',
+  cost: 'Cost',
+  status: 'Status',
+};
