e5ea1dbef5bf5f96e11db62ce85eb668c31dcfd5
andrefelizardo <andreluis.felizardo@outlook.com>
2023-11-16 15:51:12 -0300

    feat: money in and money out integrated

--- src/app/(light)/dashboard/components/wallet/action-detail.tsx
+++ src/app/(light)/dashboard/components/wallet/action-detail.tsx
@@ -8,14 +8,26 @@ export default function ActionDetail({
 }) {
   const getDetail = (): string => {
     switch (action) {
+      case FinancialTransactionAction.IssuerEarnings:
+        return transaction_actions.in.pda_revenue;
+      case FinancialTransactionAction.MoneyDeposit:
+        return transaction_actions.in.deposit;
+      case FinancialTransactionAction.MoneyWithdraw:
+        return transaction_actions.out.withdraw;
+      case FinancialTransactionAction.ProofCreate:
+        return transaction_actions.out.proof_share;
       case FinancialTransactionAction.PdaIssuance:
         return transaction_actions.out.pda_issuance;
       case FinancialTransactionAction.PdaUpdate:
         return transaction_actions.out.pda_update;
       case FinancialTransactionAction.PdaStatusChange:
         return transaction_actions.out.pda_status_change;
+      case FinancialTransactionAction.DatamodelCreate:
+        return transaction_actions.out.data_model;
+      case FinancialTransactionAction.RequestTemplateCreate:
+        return transaction_actions.out.data_request;
       default:
-        return '';
+        return action;
     }
   };
   return <>{getDetail()}</>;
