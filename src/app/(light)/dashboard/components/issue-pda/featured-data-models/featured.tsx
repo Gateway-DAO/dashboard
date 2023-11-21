1691f547f95fe54d776367e910eb7430e5c48ebb
João Conserva <boscoj@outlook.com>
2023-11-21 12:05:43 -0300

    chore: added load more button

--- src/app/(light)/dashboard/components/issue-pda/featured-data-models/featured.tsx
+++ src/app/(light)/dashboard/components/issue-pda/featured-data-models/featured.tsx
@@ -1,25 +1,37 @@
 'use client';
 
 import ExplorerFeaturedSection from '@/app/(light)/explorer/components/featured-section/featured-section';
+import { LoadingButton } from '@/components/buttons/loading-button/loading-button';
+import { queries } from '@/constants/queries';
+import { common } from '@/locale/en/common';
 import { issuePda } from '@/locale/en/pda';
 import { apiPublic } from '@/services/protocol/api';
 import { useQuery } from '@tanstack/react-query';
+
+import { Box } from '@mui/material';
 
 import DataModelCard from './data-model-card';
 
 export default function DataModelsFeatured() {
   const dataModels = useQuery({
-    queryKey: ['data-models-featured'],
+    queryKey: [queries.featured_data_models],
     queryFn: () => apiPublic.data_models_featured(),
   });
   return (
-    <ExplorerFeaturedSection
-      title={issuePda.featured}
-      isLoading={dataModels.isLoading}
-    >
-      {dataModels.data?.dataModels.map((dataModel) => (
-        <DataModelCard dataModel={dataModel} key={dataModel.id} />
-      ))}
-    </ExplorerFeaturedSection>
+    <>
+      <ExplorerFeaturedSection
+        title={issuePda.featured}
+        isLoading={dataModels.isLoading}
+      >
+        {dataModels.data?.dataModels.map((dataModel) => (
+          <DataModelCard dataModel={dataModel} key={dataModel.id} />
+        ))}
+      </ExplorerFeaturedSection>
+      <Box sx={{ mt: 4, textAlign: 'center' }}>
+        <LoadingButton variant="outlined">
+          {common.actions.load_more}
+        </LoadingButton>
+      </Box>
+    </>
   );
 }
