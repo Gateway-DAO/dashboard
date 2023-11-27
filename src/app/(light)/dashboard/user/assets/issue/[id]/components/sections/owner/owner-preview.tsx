53c986fedc6feaf586b15f17a2ef4a2726edd490
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-11-27 15:41:21 -0300

    feat: preview value

--- src/app/(light)/dashboard/user/assets/issue/[id]/components/sections/owner/owner-preview.tsx
+++ src/app/(light)/dashboard/user/assets/issue/[id]/components/sections/owner/owner-preview.tsx
@@ -1,14 +1,17 @@
-import { useDeferredValue, useEffect } from 'react';
+import { useEffect } from 'react';
 
 import { queries } from '@/constants/queries';
 import { apiPublic } from '@/services/protocol/api';
-import { UserIdentificationInput } from '@/services/protocol/types';
+import {
+  UserIdentificationInput,
+  UserIdentifierType,
+} from '@/services/protocol/types';
 import { useDebouncedState } from '@react-hookz/web';
 import { useQuery } from '@tanstack/react-query';
 import { useFormContext } from 'react-hook-form';
 
 export default function OwnerPreview() {
-  const { watch } = useFormContext();
+  const { watch, setError, clearErrors } = useFormContext();
   const owner: UserIdentificationInput = watch('owner');
   const [debouncedOwner, setDebouncedOwner] = useDebouncedState(owner, 1000);
 
@@ -19,7 +22,7 @@ export default function OwnerPreview() {
     });
   }, [owner?.type, owner?.value]);
 
-  const { data } = useQuery({
+  const { data, isSuccess, isError } = useQuery({
     queryKey: [queries.user_info, debouncedOwner?.type, debouncedOwner?.value],
     queryFn: () =>
       apiPublic.get_user_info({
@@ -33,6 +36,21 @@ export default function OwnerPreview() {
       !!debouncedOwner?.value &&
       !!debouncedOwner.value.length,
   });
+
+  useEffect(() => {
+    if (
+      isSuccess &&
+      !data?.user &&
+      debouncedOwner.type === UserIdentifierType.GatewayId
+    ) {
+      setError('owner', {
+        type: 'manual',
+        message: 'User not found',
+      });
+    } else if (isSuccess) {
+      clearErrors('owner');
+    }
+  }, [isSuccess, data]);
 
   return <div>{JSON.stringify(data)}</div>;
 }
--- src/app/(light)/dashboard/user/assets/issue/[id]/components/sections/owner/owner.tsx
+++ src/app/(light)/dashboard/user/assets/issue/[id]/components/sections/owner/owner.tsx
@@ -1,3 +1,4 @@
+import ErrorMessage from '@/components/form/error-message/error-message';
 import UserIdentityField from '@/components/form/user-identification-field/user-identifier-field';
 import { issuePdaForm } from '@/locale/en/pda';
 import { useFormContext } from 'react-hook-form';
@@ -7,7 +8,9 @@ import { Box, Paper, Stack, Typography } from '@mui/material';
 import OwnerPreview from './owner-preview';
 
 export default function OwnerSection() {
-  const { control } = useFormContext();
+  const { control, getFieldState } = useFormContext();
+
+  const { error } = getFieldState('owner');
 
   return (
     <Paper
@@ -22,13 +25,16 @@ export default function OwnerSection() {
           {issuePdaForm.issue_to.description}
         </Typography>
       </Box>
-      <UserIdentityField
-        control={control}
-        names={{
-          type: 'owner.type',
-          value: 'owner.value',
-        }}
-      />
+      <Box>
+        <UserIdentityField
+          control={control}
+          names={{
+            type: 'owner.type',
+            value: 'owner.value',
+          }}
+        />
+        {error && <ErrorMessage>{error.message}</ErrorMessage>}
+      </Box>
       <OwnerPreview />
     </Paper>
   );
