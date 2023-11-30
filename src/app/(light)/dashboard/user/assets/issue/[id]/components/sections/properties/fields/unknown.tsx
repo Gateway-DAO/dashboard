2be1fc5e34e15008107fcac7fda537ca180f0513
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-11-30 18:58:47 -0300

    feat: empty type

--- src/app/(light)/dashboard/user/assets/issue/[id]/components/sections/properties/fields/unknown.tsx
+++ src/app/(light)/dashboard/user/assets/issue/[id]/components/sections/properties/fields/unknown.tsx
@@ -9,8 +9,9 @@ import { PropertyField } from './type';
 export default function UnknownProperty({ id, ...property }: PropertyField) {
   return (
     <Typography variant="body2">
-      We currently don't support the field type "{property.type}" on the no-code
-      issuance. Please use the{' '}
+      We currently don't support the field type{' '}
+      <b>"{property.type ?? 'empty'}"</b> for the field <b>"{id}"</b> on the
+      no-code issuance. Please use the{' '}
       <MuiLink component={Link} href={routes.dashboard.user.developerAccess}>
         API
       </MuiLink>{' '}
