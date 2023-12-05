9df9099b3020100a3acb9b470f03dc4143300d8d
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-12-05 12:09:01 -0300

    feat: update wallet value

--- src/app/(light)/dashboard/components/issue-pda/issue-form/form/preview/preview.tsx
+++ src/app/(light)/dashboard/components/issue-pda/issue-form/form/preview/preview.tsx
@@ -4,10 +4,11 @@ import { useParams, useRouter } from 'next/navigation';
 import IssuanceSuccess from '@/app/(light)/dashboard/components/issue-pda/success/success';
 import ModalTitle from '@/components/modal/modal-header/modal-header';
 import ModalRight from '@/components/modal/modal-right/modal-right';
+import { queries } from '@/constants/queries';
 import routes from '@/constants/routes';
 import { useGtwSession } from '@/context/gtw-session-provider';
 import useOrganization from '@/hooks/use-organization';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useSnackbar } from 'notistack';
 
 import { IssuePdaSchema } from '../schema';
@@ -20,8 +21,8 @@ export default function Preview({
   ...props
 }: PreviewModalProps) {
   const { id } = useParams();
-  const { privateApi } = useGtwSession();
-
+  const { privateApi, session } = useGtwSession();
+  const queryClient = useQueryClient();
   const { organization } = useOrganization();
   const router = useRouter();
   const { mutateAsync, isLoading, isSuccess, data } = useMutation({
@@ -40,6 +41,10 @@ export default function Preview({
   const onSubmit = async () => {
     try {
       await mutateAsync(props.data);
+      await queryClient.invalidateQueries([
+        queries.my_wallet,
+        organization ? organization.id : session?.user.id,
+      ]);
     } catch (error: any) {
       if (error?.response?.data?.message) {
         enqueueSnackbar(error.response.data.message, { variant: 'error' });
