296bc59e1abaad593a2a985a9943a85d33e7fba4
Siddharth9890 <siddharth.a9890@gmail.com>
2023-12-07 09:47:50 +0530

    chore: removed unused imports

--- src/app/(light)/dashboard/components/sidebar/instruction/instruction.tsx
+++ src/app/(light)/dashboard/components/sidebar/instruction/instruction.tsx
@@ -2,10 +2,6 @@
 import QuestionSquaredIcon from '@/components/icons/question-squared';
 import ModalHeader from '@/components/modal/modal-header/modal-header';
 import ModalRight from '@/components/modal/modal-right/modal-right';
-import { queries } from '@/constants/queries';
-import { apiPublic } from '@/services/protocol/api';
-import { Explorer_Data_Model_Detail_OverviewQuery } from '@/services/protocol/types';
-import { useQuery } from '@tanstack/react-query';
 import InstructionHeader from './header';
 
 type Props = {
