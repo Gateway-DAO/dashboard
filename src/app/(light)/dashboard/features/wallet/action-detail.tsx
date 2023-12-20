4dcafdce6d61cd1d7a853ac42db677360eb942e3
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-12-20 11:59:00 -0300

    chore: simplify action detail

--- src/app/(light)/dashboard/features/wallet/action-detail.tsx
+++ src/app/(light)/dashboard/features/wallet/action-detail.tsx
@@ -1,36 +1,33 @@
 import { financial_transaction_actions } from '@/locale/en/transaction';
 import { FinancialTransactionAction } from '@/services/protocol/types';
 
+const actions = {
+  [FinancialTransactionAction.IssuerEarnings]:
+    financial_transaction_actions.in.pda_revenue,
+  [FinancialTransactionAction.MoneyDeposit]:
+    financial_transaction_actions.in.deposit,
+  [FinancialTransactionAction.MoneyWithdraw]:
+    financial_transaction_actions.out.withdraw,
+  [FinancialTransactionAction.ProofCreate]:
+    financial_transaction_actions.out.proof_share,
+  [FinancialTransactionAction.PdaIssuance]:
+    financial_transaction_actions.out.pda_issuance,
+  [FinancialTransactionAction.PdaUpdate]:
+    financial_transaction_actions.out.pda_update,
+  [FinancialTransactionAction.PdaStatusChange]:
+    financial_transaction_actions.out.pda_status_change,
+  [FinancialTransactionAction.DatamodelCreate]:
+    financial_transaction_actions.out.data_model,
+  [FinancialTransactionAction.RequestTemplateCreate]:
+    financial_transaction_actions.out.data_request_template,
+  [FinancialTransactionAction.RequestCreate]:
+    financial_transaction_actions.out.request_cost,
+};
+
 export default function FinancialActionDetail({
   action,
 }: {
   action: FinancialTransactionAction;
 }) {
-  const getDetail = (): string => {
-    switch (action) {
-      case FinancialTransactionAction.IssuerEarnings:
-        return financial_transaction_actions.in.pda_revenue;
-      case FinancialTransactionAction.MoneyDeposit:
-        return financial_transaction_actions.in.deposit;
-      case FinancialTransactionAction.MoneyWithdraw:
-        return financial_transaction_actions.out.withdraw;
-      case FinancialTransactionAction.ProofCreate:
-        return financial_transaction_actions.out.proof_share;
-      case FinancialTransactionAction.PdaIssuance:
-        return financial_transaction_actions.out.pda_issuance;
-      case FinancialTransactionAction.PdaUpdate:
-        return financial_transaction_actions.out.pda_update;
-      case FinancialTransactionAction.PdaStatusChange:
-        return financial_transaction_actions.out.pda_status_change;
-      case FinancialTransactionAction.DatamodelCreate:
-        return financial_transaction_actions.out.data_model;
-      case FinancialTransactionAction.RequestTemplateCreate:
-        return financial_transaction_actions.out.data_request_template;
-      case FinancialTransactionAction.RequestCreate:
-        return financial_transaction_actions.out.request_cost;
-      default:
-        return action;
-    }
-  };
-  return <>{getDetail()}</>;
+  return <>{actions?.[action as keyof typeof actions] ?? action}</>;
 }
