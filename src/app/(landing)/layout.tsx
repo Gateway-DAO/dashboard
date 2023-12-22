b2d73bb4261da3aa033cac05f3eefa420eccbb05
andrefelizardo <andreluis.felizardo@outlook.com>
2023-12-22 13:15:13 -0300

    fix gtm script on landing

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -26,6 +26,19 @@ export default function RootLayout({
         <>
           <Script
             strategy="afterInteractive"
+            id="ga-landing"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N32L27DH');
+          `,
+            }}
+          />
+          {/* <Script
+            strategy="afterInteractive"
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_TAG}`}
           />
           <Script
@@ -40,7 +53,7 @@ export default function RootLayout({
                 gtag('config', '${process.env.NEXT_PUBLIC_GTM_TAG}');
               `,
             }}
-          />
+          /> */}
         </>
       )}
       <body>
