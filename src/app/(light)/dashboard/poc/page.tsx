30f4d64a32fe8fb878f1c767da93e0d1815f30d8
João Conserva <boscoj@outlook.com>
2023-12-28 18:03:31 -0300

    chore: updated poc href

--- src/app/(light)/dashboard/poc/page.tsx
+++ src/app/(light)/dashboard/poc/page.tsx
@@ -8,7 +8,7 @@ export default function POCHome() {
       </Typography>
       <Button
         variant="contained"
-        href="https://widget-poc-one.vercel.app/issue?access=12345&gtwid=joao&owner=kbooz&datamodel=12345-12345-12345-12345&claim={value:1,name:%20%27junior%27}&callback=https://mygateway.xyz/"
+        href="https://widget-poc-one.vercel.app/issue?access=5263b875-a216-473b-b4da-d674313c6642&gtwid=visajames&owner=kbooz&datamodel=12345-12345-12345-12345&claim={value:1,name:%20%27junior%27}&callback=https://mygateway.xyz/dashboard/poc"
       >
         Issue
       </Button>
