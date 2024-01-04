e37716fa41dde0a43e9ccee6288f817865a0b3d5
João Conserva <boscoj@outlook.com>
2024-01-04 14:43:31 -0300

    chore: updated claim value

--- src/app/(light)/dashboard/poc/issue/page.tsx
+++ src/app/(light)/dashboard/poc/issue/page.tsx
@@ -22,17 +22,12 @@ export default function POCIssueHome() {
       wallets[0]?.data?.address ??
       emails[0]?.data?.address ??
       'tullio%40mygateway.xyz',
-    dataModel: '6f61ccb0-85e0-47b3-879d-f197c04c4f9e',
-    claim:
-      '%7B%22tier%22%3A%22Gold%22%2C%22points%22%3A1000%2C%22type%22%3A%22Adventure+Expert%22%7D',
+    dataModel: 'a6bc5bcc-a3f4-4a7c-ab2f-e2597bfbc07a',
+    claim: '{%22name%22:"%22test%22}',
     callback: 'https://dev.mygateway.xyz/dashboard/poc/issue',
   };
 
-  const {
-    mutateAsync: generateSession,
-    data,
-    isLoading,
-  } = useMutation({
+  const { mutateAsync: generateSession, isLoading } = useMutation({
     mutationKey: ['generate-session', { ...params }],
     mutationFn: async () => {
       const response = await fetch(
