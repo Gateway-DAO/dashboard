ab8c67f6ceecd593ed9013de9807a8d9a831ac30
João Conserva <boscoj@outlook.com>
2024-01-23 18:26:45 -0300

    chore: created methods to call generate widget session

--- src/components/educational/components/educational-modal-content.tsx
+++ src/components/educational/components/educational-modal-content.tsx
@@ -1,15 +1,19 @@
 'use client';
 
+import { useRouter } from 'next-nprogress-bar';
 import { FC } from 'react';
 
+import { GenerateIssueBody } from '@/app/api/generate-issue-session-educational/route';
+import { LoadingButton } from '@/components/buttons/loading-button/loading-button';
 import BenefitsIcon from '@/components/icons/benefits';
 import JoinClickIcon from '@/components/icons/join-click';
 import PoolTogetherIcon from '@/components/icons/pool-together';
-import { educationalKeys } from '@/constants/educational';
-import useEducational from '@/hooks/use-educational';
+import { mutations } from '@/constants/queries';
+import { useGtwSession } from '@/context/gtw-session-provider';
 import { instructionGuide } from '@/locale/en/educational';
+import { useMutation } from '@tanstack/react-query';
 
-import { Button, Stack, SvgIconProps, Typography } from '@mui/material';
+import { Stack, SvgIconProps, Typography } from '@mui/material';
 
 import EducationalModalCards from './educational-modal-cards';
 
@@ -18,7 +22,45 @@ type Props = {
 };
 
 export default function EducationalModalContent({ onClose }: Props) {
-  const { setEducational } = useEducational();
+  const { session } = useGtwSession();
+  const router = useRouter();
+
+  const sessionProps: GenerateIssueBody = {
+    claim: {
+      avatar: session.user.profilePicture ?? '',
+      name: session.user.displayName,
+      username: session.user.gatewayId,
+      email: session.user.email,
+      evmWallet: 'evmWallet',
+      solanaWallet: 'solanaWallet',
+    },
+  };
+
+  const { mutateAsync: onGenerateSession, isLoading } = useMutation({
+    mutationKey: [
+      mutations.generate_issue_session_educational,
+      session.user.id,
+    ],
+    mutationFn: async () => {
+      const response = await fetch('/api/generate-issue-session-educational', {
+        method: 'POST',
+        body: JSON.stringify(sessionProps),
+      });
+      const data = await response.json();
+      if (data.error || !data.session) {
+        console.error('Error to issue PDA', data.error);
+        throw new Error('Failed to issue PDA');
+      }
+      return data;
+    },
+  });
+
+  const generateSession = async () => {
+    const { session } = await onGenerateSession();
+    localStorage.setItem('widget-educational-session', session.sessionId);
+    onClose();
+    router.push(session.url);
+  };
 
   const cards: { title: string; icon: FC<SvgIconProps> }[] = [
     {
@@ -53,18 +95,13 @@ export default function EducationalModalContent({ onClose }: Props) {
       <Stack direction="row" gap={1} mb={5}>
         <EducationalModalCards cards={cards} />
       </Stack>
-      <Button
+      <LoadingButton
         variant="contained"
-        onClick={() => {
-          setEducational({
-            key: educationalKeys.start_using_pda,
-            value: '31181920-6eff-4314-a8ea-0177a848ebca',
-          });
-          onClose();
-        }}
+        isLoading={isLoading}
+        onClick={generateSession}
       >
         {instructionGuide.claim_your_first_pda.btn_text}
-      </Button>
+      </LoadingButton>
     </Stack>
   );
 }
