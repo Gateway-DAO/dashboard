15373c69dc2199e83165d287cae208501d23cb70
Lucas Inacio <lucas1inacio7souza@gmail.com>
2024-02-21 16:09:35 -0300

    feat: split server and client providers

--- src/app/(landing)/providers.tsx
+++ src/app/(landing)/providers.tsx
@@ -1,21 +1,13 @@
-'use client';
-
-import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 import { PropsWithChildren } from 'react';
 
 import { ThemeProvider } from '@/theme';
-import { ReactLenis } from '@studio-freight/react-lenis';
+
+import ClientProviders from './client-provider';
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
     <ThemeProvider dark>
-      <ProgressBar
-        height="4px"
-        color="#771AC9"
-        options={{ showSpinner: false }}
-        shallowRouting
-      />
-      <ReactLenis root>{children}</ReactLenis>
+      <ClientProviders>{children}</ClientProviders>
     </ThemeProvider>
   );
 }
--- src/theme/theme-provider.tsx
+++ src/theme/theme-provider.tsx
@@ -1,10 +1,6 @@
-'use client';
-import { useServerInsertedHTML } from 'next/navigation';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren } from 'react';
 
-import createCache from '@emotion/cache';
-import { CacheProvider } from '@emotion/react';
-
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
 
@@ -25,59 +21,13 @@ export function ThemeProvider({
 }: PropsWithChildren<Props>) {
   const theme = dark ? darkTheme : lightTheme;
 
-  const [{ cache, flush }] = useState(() => {
-    const cache = createCache({ key: 'gtw' });
-    cache.compat = true;
-    const prevInsert = cache.insert;
-    let inserted: string[] = [];
-    cache.insert = (...args) => {
-      const serialized = args[1];
-      if (cache.inserted[serialized.name] === undefined) {
-        inserted.push(serialized.name);
-      }
-      return prevInsert(...args);
-    };
-    const flush = () => {
-      const prevInserted = inserted;
-      inserted = [];
-      return prevInserted;
-    };
-    return { cache, flush };
-  });
-
-  useServerInsertedHTML(() => {
-    const names = flush();
-    if (names.length === 0) {
-      return null;
-    }
-    let styles = '';
-    for (const name of names) {
-      styles += cache.inserted[name];
-    }
-    return (
-      <style
-        key={cache.key}
-        data-emotion={`${cache.key} ${names.join(' ')}`}
-        dangerouslySetInnerHTML={{
-          __html: styles,
-        }}
-      />
-    );
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'development') {
-      (window as any).theme = theme;
-    }
-  }, [theme]);
-
   return (
-    <CacheProvider value={cache}>
+    <AppRouterCacheProvider>
       <MUIThemeProvider theme={theme}>
         {withGlobalStyles && <GlobalStyles />}
         <CssBaseline />
         {children}
       </MUIThemeProvider>
-    </CacheProvider>
+    </AppRouterCacheProvider>
   );
 }
