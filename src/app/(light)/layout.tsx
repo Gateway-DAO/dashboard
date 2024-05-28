b15434b44f11989420c23f6008bd23cedd182e53
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-05-28 18:33:31 -0300

    chore: reverted old and v3 folder to default

--- src/app/(light)/layout.tsx
+++ src/app/(light)/layout.tsx
@@ -23,23 +23,6 @@ export default function RootLayout({
 }) {
   const env = currentEnv;
   const isTesnetOrProd = env === 'testnet' || env === 'production';
-  const hotjarScript = DOMPurify.sanitize(`
-  (function(h,o,t,j,a,r){
-      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-      h._hjSettings={hjid:3399024,hjsv:6};
-      a=o.getElementsByTagName('head')[0];
-      r=o.createElement('script');r.async=1;
-      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-      a.appendChild(r);
-  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-  `);
-  const gtmScript = DOMPurify.sanitize(`
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-N32L27DH');
-`);
 
   return (
     <html lang="en">
@@ -49,7 +32,16 @@ export default function RootLayout({
           id="hotjar"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: hotjarScript,
+            __html: DOMPurify.sanitize(`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3399024,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `),
           }}
         />
       )}
@@ -59,7 +51,13 @@ export default function RootLayout({
           <Script
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
-              __html: gtmScript,
+              __html: DOMPurify.sanitize(`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N32L27DH');
+            `),
             }}
           />
           {/* <Script
