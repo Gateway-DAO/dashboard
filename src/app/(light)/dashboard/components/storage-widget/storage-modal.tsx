b068063878c3dd78ff883bcd77c28b8769277e70
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-06-14 18:50:20 -0300

    feat: storage widget and modal

---
+++ src/app/(light)/dashboard/components/storage-widget/storage-modal.tsx
@@ -0,0 +1,34 @@
+import ModalHeader from '@/components/modal/modal-header/modal-header';
+import ModalRight from '@/components/modal/modal-right/modal-right';
+
+import LaunchIcon from '@mui/icons-material/Launch';
+import { Button, Typography } from '@mui/material';
+type Props = {
+  isOpen: boolean;
+  toggle: () => void;
+};
+
+export default function StorageModal({ isOpen, toggle }: Props) {
+  return (
+    <ModalRight open={isOpen} onClose={toggle}>
+      <ModalHeader onClose={toggle} />
+      <Typography variant="h4" mb={3}>
+        Upgrade storage (soon)
+      </Typography>
+      <Typography variant="body1" mb={3}>
+        We're currently in the process of implementing this feature. As part of
+        our ongoing efforts, we're conducting a brief research on storage
+        upgrades. It consists of just two questions. Would you like to assist
+        us?
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={toggle}
+        startIcon={<LaunchIcon />}
+      >
+        Answer questions
+      </Button>
+    </ModalRight>
+  );
+}
--- src/app/(light)/dashboard/components/storage-widget/storage-widget.tsx
+++ src/app/(light)/dashboard/components/storage-widget/storage-widget.tsx
@@ -3,75 +3,103 @@
 import { useSession } from 'next-auth/react';
 
 import { convertBytes, formatBytes } from '@/utils/bytes';
+import { useToggle } from '@react-hookz/web';
 
-import { Box, CircularProgress, Stack, Typography, alpha } from '@mui/material';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Stack,
+  Typography,
+  alpha,
+} from '@mui/material';
+
+import StorageModal from './storage-modal';
 
 const MAX_STORAGE = 2147483648 / 2;
 
 export default function StorageWidget() {
+  const [isOpen, toggle] = useToggle();
   const session = useSession();
 
   return (
-    <Box
-      sx={(theme) => ({
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.focusOpacity
-        ),
-        borderRadius: theme.shape.borderRadius / 16, //Strange issue with MUI
-        p: 2,
-        textAlign: 'left',
-      })}
-    >
-      <Stack direction="row" gap={2}>
-        <Box
-          sx={{
-            position: 'relative',
-          }}
-        >
-          <CircularProgress
-            {...(session.status === 'authenticated' && {
-              variant: 'determinate',
-              value: (session.data.user.totalFileSize / MAX_STORAGE) * 100,
-            })}
+    <>
+      <Box
+        sx={(theme) => ({
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.focusOpacity
+          ),
+          borderRadius: theme.shape.borderRadius / 16, //Strange issue with MUI
+          p: 2,
+          textAlign: 'left',
+        })}
+      >
+        <Stack direction="row" gap={2}>
+          <Box
             sx={{
-              position: 'absolute',
-              zIndex: 2,
-              circle: {
-                strokeLinecap: 'round',
-              },
+              position: 'relative',
             }}
-          />
-          <CircularProgress
-            variant="determinate"
-            value={100}
-            sx={{
-              opacity: 0.3,
-            }}
-          />
-        </Box>
-        <Stack>
-          <Typography variant="caption" color="primary.dark">
-            Storage
-          </Typography>
-          <Typography variant="caption" color="primary.dark" fontWeight="bold">
-            {convertBytes(session.data?.user.totalFileSize ?? 0, 'GB').toFixed(
-              2
-            )}{' '}
-            <Typography
-              component="span"
-              color="primary.dark"
-              fontWeight="lighter"
-              variant="caption"
+          >
+            <CircularProgress
+              {...(session.status === 'authenticated' && {
+                variant: 'determinate',
+                value: (session.data.user.totalFileSize / MAX_STORAGE) * 100,
+              })}
               sx={{
-                opacity: 0.45,
+                position: 'absolute',
+                zIndex: 2,
+                circle: {
+                  strokeLinecap: 'round',
+                },
               }}
-            >
-              / {formatBytes(MAX_STORAGE)}
+            />
+            <CircularProgress
+              variant="determinate"
+              value={100}
+              sx={{
+                opacity: 0.3,
+              }}
+            />
+          </Box>
+          <Stack>
+            <Typography variant="caption" color="primary.dark">
+              Storage
             </Typography>
-          </Typography>
+            <Typography
+              variant="caption"
+              color="primary.dark"
+              fontWeight="bold"
+            >
+              {convertBytes(
+                session.data?.user.totalFileSize ?? 0,
+                'GB'
+              ).toFixed(2)}{' '}
+              <Typography
+                component="span"
+                color="primary.dark"
+                fontWeight="lighter"
+                variant="caption"
+                sx={{
+                  opacity: 0.45,
+                }}
+              >
+                / {formatBytes(MAX_STORAGE)}
+              </Typography>
+            </Typography>
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
+        <Button
+          onClick={toggle}
+          fullWidth
+          variant="outlined"
+          size="small"
+          sx={{ mt: 1 }}
+        >
+          Upgrade
+        </Button>
+      </Box>
+      <StorageModal isOpen={isOpen} toggle={toggle} />
+    </>
   );
 }
