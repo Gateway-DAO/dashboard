5139b4bbf61bbab78db8fd2d09ae03224f2b81fc
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-06-19 15:34:11 -0300

    feat: removed issue form

--- src/app/(light)/dashboard/features/issue-pda/issue-form/form/sections/properties/fields/type.ts
+++
@@ -1,7 +0,0 @@
-import { SchemaProperty } from '@/utils/get-claim-type';
-
-export type PropertyField = {
-  id: string;
-  defaultValue?: any;
-  hideHelperText?: boolean;
-} & SchemaProperty;
--- src/app/(light)/dashboard/features/issue-pda/issue-form/form/sections/properties/type.ts
+++
@@ -1,7 +0,0 @@
-import { SchemaProperty } from '@/utils/get-claim-type';
-
-export type PropertyProps = {
-  id: string;
-  property: SchemaProperty;
-  required?: boolean;
-};
