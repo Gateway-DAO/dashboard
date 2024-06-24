5a67f3dc1b90b644f0184cfa7e22c9bf2f2845f7
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-06-24 16:38:07 -0300

    fix

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,12 +1,5 @@
 import { DecryptedData } from '@/services/next-auth/libs/get-decrypted-data';
 import { MeQuery } from '@/services/protocol-v3/types';
-import { Login_DataFragment } from '@/services/protocol/types';
-
-// TODO: Remove after V3
-export type SessionToken = Login_DataFragment & {
-  error?: any;
-  skipEmail?: boolean;
-};
 
 type UserV3Data = Omit<MeQuery, 'me'> & {
   user: MeQuery['me'];
