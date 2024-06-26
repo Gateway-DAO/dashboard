c4a433e9ca4759ee750cee3a10dffe41beb83eaa
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-06-26 18:47:49 -0300

    fix: lenis

--- src/app/(landing)/components/document/index.tsx
+++ src/app/(landing)/components/document/index.tsx
@@ -1,17 +1,16 @@
-import styles from './document.module.scss';
-import Wrapper from '../wrapper';
 import { useLenis } from '@studio-freight/react-lenis';
 
-export default function Document({ children }: { children: React.ReactNode}) {
+import Wrapper from '../wrapper';
+import styles from './document.module.scss';
+
+export default function Document({ children }: { children: React.ReactNode }) {
   const lenis = useLenis();
 
-  lenis.scrollTo(0, { immediate: true });
+  lenis?.scrollTo(0, { immediate: true });
 
   return (
     <div className={styles.element}>
-      <Wrapper>
-        {children}
-      </Wrapper>
+      <Wrapper>{children}</Wrapper>
     </div>
-  )
+  );
 }
