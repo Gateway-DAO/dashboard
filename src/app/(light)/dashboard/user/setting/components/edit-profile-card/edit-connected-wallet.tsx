89bf58d5ad7f24b692739e2bab499ff85295db4d
manish <github.jmtss@gmail.com>
2024-08-01 17:12:20 +0530

    chore: build and setup components

--- src/app/(light)/dashboard/user/setting/components/edit-profile-card/edit-connected-wallet.tsx
+++ src/app/(light)/dashboard/user/setting/components/edit-profile-card/edit-connected-wallet.tsx
@@ -1,7 +1,39 @@
+import AddIcon from '@mui/icons-material/Add';
+import { Button, Stack, Typography } from '@mui/material';
+
 export function EditConnectedWallet() {
   return (
-    <>
-      <h1>edit conneceted wallet section</h1>
-    </>
+    <Stack
+      width={591}
+      height="auto"
+      bgcolor={'white'}
+      padding={2}
+      borderRadius={1.2}
+    >
+      <Stack
+        direction={'row'}
+        justifyContent={'space-between'}
+        alignItems={'center'}
+        marginBottom={4}
+      >
+        <Stack>
+          <Typography variant="subtitle1">Connected wallet</Typography>
+          <Typography variant="body2">
+            Connected wallets to your Gateway ID
+          </Typography>
+        </Stack>
+        <Stack>
+          <Button variant="outlined" startIcon={<AddIcon />}>
+            Add
+          </Button>
+        </Stack>
+      </Stack>
+      <Stack>
+        <Typography variant="body1">
+          5eFZRRRa74j9QZ7NnUoshpuLrPrWVgKJgXanLXX4ceYn
+        </Typography>
+        <Typography variant="caption">Solana</Typography>
+      </Stack>
+    </Stack>
   );
 }
--- src/app/(light)/dashboard/user/setting/components/edit-profile-card/edit-profile-card.tsx
+++ src/app/(light)/dashboard/user/setting/components/edit-profile-card/edit-profile-card.tsx
@@ -1,7 +1,36 @@
+import EditIcon from '@mui/icons-material/Edit';
+import { Avatar, Badge, IconButton, Paper, Stack } from '@mui/material';
+
+import { EditMetaInfo } from './components/edit-meta-info';
+
 export function EditProfileCard() {
   return (
     <>
-      <h1>hello</h1>
+      <Stack
+        width={591}
+        height="auto"
+        bgcolor={'primary.light'}
+        padding={2}
+        borderRadius={1.2}
+      >
+        <Badge
+          overlap="circular"
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          badgeContent={
+            <IconButton size="small">
+              <EditIcon fontSize="small" />
+            </IconButton>
+          }
+        >
+          <Avatar
+            sx={{ width: 60, height: 60 }}
+            alt="Edit Picture"
+            src="avatar.png"
+          />
+        </Badge>
+
+        <EditMetaInfo />
+      </Stack>
     </>
   );
 }
