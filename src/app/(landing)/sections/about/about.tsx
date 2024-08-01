90ba52aa0af0e004ba68c78204eb84830c1831f2
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-08-01 16:32:17 -0300

    feat: whole card clickable

--- src/app/(landing)/sections/about/about.tsx
+++ src/app/(landing)/sections/about/about.tsx
@@ -3,7 +3,15 @@ import Link from 'next/link';
 
 import NavigationImage from 'public/images/navigation.jpg';
 
-import { Box, Button, Card, Container, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  Container,
+  Stack,
+  Typography,
+} from '@mui/material';
 
 import AboutCards from './about-cards';
 
@@ -33,45 +41,57 @@ export default function About() {
               xs: 'unset',
               sm: '50%',
             },
-            p: 4,
-            justifyContent: 'space-between',
-            gap: {
-              xs: 4,
-              md: 26,
-            },
           }}
         >
-          <Stack gap={1}>
-            <Typography component="h3" variant="subtitle1" color="primary.main">
-              For Business: Give Data Utility
-            </Typography>
-            <Typography
-              component="h2"
-              variant="h3"
-              sx={{
-                typography: {
-                  xs: 'h5',
-                  sm: 'h4',
-                  md: 'h3',
-                },
-              }}
-            >
-              Encrypt, store, and power user-controlled data sharing with
-              Gateway
-            </Typography>
-            <Typography variant="body1">
-              A private and powerful computer for sensitive information.
-            </Typography>
-          </Stack>
-          <Button
+          <CardActionArea
             component={Link}
             href="#"
-            size="large"
-            variant="contained"
-            sx={{ alignSelf: 'flex-start' }}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              p: 4,
+              justifyContent: 'space-between',
+              gap: {
+                xs: 4,
+                md: 26,
+              },
+            }}
           >
-            Start building
-          </Button>
+            <Stack gap={1}>
+              <Typography
+                component="h3"
+                variant="subtitle1"
+                color="primary.main"
+              >
+                For Business: Give Data Utility
+              </Typography>
+              <Typography
+                component="h2"
+                variant="h3"
+                sx={{
+                  typography: {
+                    xs: 'h5',
+                    sm: 'h4',
+                    md: 'h3',
+                  },
+                }}
+              >
+                Encrypt, store, and power user-controlled data sharing with
+                Gateway
+              </Typography>
+              <Typography variant="body1">
+                A private and powerful computer for sensitive information.
+              </Typography>
+            </Stack>
+            <Button
+              component="span"
+              size="large"
+              variant="contained"
+              sx={{ alignSelf: 'flex-start' }}
+            >
+              Start building
+            </Button>
+          </CardActionArea>
         </Stack>
         <Card
           variant="outlined"
