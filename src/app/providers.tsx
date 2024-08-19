dd249a8a99f50814480c6ed7797d5fddad89e73d
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-08-19 14:23:18 -0300

    feat: unified providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,33 @@
-import { PropsWithChildren } from 'react';
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+import { AppProgressBar } from 'next-nprogress-bar';
+import { PropsWithChildren, useState } from 'react';
 
 import Notistack from '@/components/notistack/notistack';
+import { queryClientConfig } from '@/services/query-client';
 import { ThemeProvider } from '@/theme';
-
-import ClientProviders from './client-provider';
-
-// TODO: pass session from Layout
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function Providers({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient(queryClientConfig));
+
   return (
-    <ClientProviders>
-      <ThemeProvider>
-        <Notistack>{children}</Notistack>
-      </ThemeProvider>
-    </ClientProviders>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider refetchOnWindowFocus refetchInterval={5000}>
+          <AppProgressBar
+            height="4px"
+            color="#771AC9"
+            options={{ showSpinner: false }}
+            shallowRouting
+          />
+          <ReactQueryDevtools initialIsOpen={false} />
+
+          <Notistack>{children}</Notistack>
+        </SessionProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
