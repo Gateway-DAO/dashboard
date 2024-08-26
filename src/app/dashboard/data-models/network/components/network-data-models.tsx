2b064582ea425fb6645eef17a7d7e00e40ad7e7e
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-08-26 18:14:43 -0300

    chore: public endpoint

--- src/app/dashboard/data-models/network/components/network-data-models.tsx
+++ src/app/dashboard/data-models/network/components/network-data-models.tsx
@@ -1,20 +1,14 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
-
-import { clientApi, createAuthHeader } from '@/services/api/client';
+import { clientApi } from '@/services/api/client';
 import { DataModel } from '@/services/api/models';
 
 import DataModelList from '../../components/data-model-list';
 
 export default function NetworkDataModels() {
-  const { data: session } = useSession({ required: true });
-
   const { isLoading, isSuccess, data } = clientApi.useQuery(
     'get',
-    '/data-models',
-    { headers: createAuthHeader(session?.token) },
-    { enabled: !!session }
+    '/data-models'
   );
 
   return (
