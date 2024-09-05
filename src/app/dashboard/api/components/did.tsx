5efae7f3d02acdff0916ac2f89e46a37bed69097
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-09-05 14:40:04 -0300

    feat: show did on api

--- src/app/dashboard/api/components/did.tsx
+++ src/app/dashboard/api/components/did.tsx
@@ -1,15 +1,8 @@
-'use client';
-import { useSession } from 'next-auth/react';
-
 import CopyButton from '@/components/copy-button/copy-button';
-import ToggleVisibilityButton from '@/components/toggle-visibility-button/toggle-visibility-button';
-import { useToggle } from '@react-hookz/web';
 
 import { Card, CardContent, CardHeader, Typography } from '@mui/material';
 
 export default function DID({ did }: { did: string }) {
-  const [isVisible, toggleVisible] = useToggle(false);
-
   return (
     <Card sx={{ width: '100%' }} variant="outlined">
       <CardHeader
@@ -18,22 +11,10 @@ export default function DID({ did }: { did: string }) {
           variant: 'subtitle1',
           fontWeight: 'bold',
         }}
-        action={
-          <>
-            <ToggleVisibilityButton
-              isVisible={isVisible}
-              onToggle={toggleVisible}
-              size="medium"
-              sx={{ mr: 1 }}
-            />
-            <CopyButton text={did} size="medium" />
-          </>
-        }
+        action={<CopyButton text={did} size="medium" />}
       />
       <CardContent>
-        <Typography sx={{ mb: 2, wordBreak: 'break-all' }}>
-          {isVisible ? did : '••••'}
-        </Typography>
+        <Typography sx={{ wordBreak: 'break-all' }}>{did}</Typography>
       </CardContent>
     </Card>
   );
