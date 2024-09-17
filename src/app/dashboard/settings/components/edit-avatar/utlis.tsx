25d6741d736741c77720e7b8429290b29c7e6f3c
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-09-17 15:31:59 -0300

    feat: enable any kind of image

--- src/app/dashboard/settings/components/edit-avatar/utlis.tsx
+++ src/app/dashboard/settings/components/edit-avatar/utlis.tsx
@@ -3,15 +3,11 @@ import { Account } from '@/services/api/models';
 export const readImageFile = (files: FileList | File[]) => {
   const reader = new FileReader();
   const file = files[0];
-  const imageTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
 
   return new Promise<{ image: string; file: Blob; isGif: boolean }>(
     (resolve, reject) => {
       if (!file) {
         return reject('No file selected');
-      }
-      if (!imageTypes.includes(file.type)) {
-        return reject('Only JPG,PNG,GIF,SVG are allowed');
       }
       if (file.size > 5245329 && file.type.includes('gif')) {
         return reject('File size should be less than 5 mb');
