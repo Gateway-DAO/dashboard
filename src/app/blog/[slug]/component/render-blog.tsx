77983187aeddfe03852a758fc39fac0c309f6858
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-10-04 13:05:35 -0300

    feat: zoom image on click

--- src/app/blog/[slug]/component/render-blog.tsx
+++ src/app/blog/[slug]/component/render-blog.tsx
@@ -14,6 +14,35 @@ export function RenderBlog({ renderHtml }: Props) {
     <>
       <Box dangerouslySetInnerHTML={{ __html: renderHtml }}></Box>
       <Script src="/js/video.js" />
+      <Script
+        id="image-script"
+        dangerouslySetInnerHTML={{
+          __html: `
+            const images = document.querySelectorAll('.kg-image');
+            images.forEach((image) => {
+                image.tabIndex = 0;
+                // add button role to image
+                image.setAttribute('role', 'button');
+                // add aria-label to image
+                image.setAttribute('aria-label', 'Expand image in new tab');
+
+                const openImage = () => {
+                  window.open(image.src, '_blank');
+                };
+
+                // opens image url in new tab
+                image.addEventListener('click', openImage);
+
+                // open image url in new tab on enter key
+                image.addEventListener('keydown', (e) => {
+                  if (e.key === 'Enter') {
+                    openImage()
+                  }
+                });
+            });
+          `,
+        }}
+      />
     </>
   );
 }
